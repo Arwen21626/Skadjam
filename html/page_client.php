609767fa8078fba7fc6a19627b00a0016cb3ad52
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-04 16:42:06 +0100

    Add session check and client info retrieval

--- html/page_client.php
+++ html/page_client.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,12 +9,25 @@
 </head>
 <body>
     <?php
-        include "connexion_client.php";
+        if (empty($_SESSION['idCompte'])) {
+            // client non connecté -> redirection vers connexion
+            header('Location: connexion_client.php');
+            exit;
+        }
+
+        $id = (int) $_SESSION['idCompte'];
+
         try{
             $dbh = new PDO("$driver:host=$server;dbname=$dbname",$user,$pass);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-            
+            $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            // Récupérer toutes les infos du client
+            $infosClient = $dbh->prepare('SELECT idCompte, nomCompte, prenomCompte, adresseMail, numeroTelephone FROM compte WHERE idCompte = :idCompte LIMIT 1');
+            $infosClient->execute([':idCompte' => $id]);
+            $client = $infosClient->fetch(PDO::FETCH_ASSOC);
+
+            $dbh = null;
         }catch(PDOException $e){
             print "Erreur : " . $e->getMessage() . "<br>" ;
             die();
@@ -23,11 +37,11 @@
     <article>
         <section>
             <img src="<?php echo $photo ?>" alt="Photo de profil" width="50" height="50">
-            <h3><?php echo $nom ?></h3>
-            <h3><?php echo $prenom ?></h3>
+            <h3><?php echo $client['nomCompte'] ?></h3>
+            <h3><?php echo $client['prenomCompte'] ?></h3>
         </section>
-        <h3><?php echo $telephone ?></h3>
-        <h3><?php echo $mail ?></h3>
+        <h3><?php echo $client['numeroTelephone'] ?></h3>
+        <h3><?php echo $client['adresseMail'] ?></h3>
     </article>
     <article>
         <form action="modifier_client.php"><input type="submit" value="Modifier"></form>
