448a6aebe037a85e6c2301684eac3e505f70eb06
Diwezha <diwezha.ruveron@gmail.com>
2025-11-04 10:14:21 +0100

    création du fichier pour les vérifications des champ dans les formulaires et fin du formulaire (html) de création d'un compte client

--- html/creationCompteClient.php
+++ html/creationCompteClient.php
@@ -8,11 +8,36 @@
 <body>
     <h2>Création du compte client</h2>
 
-    <form action=".php" method="post">
-        <label for="nom">Nom</label>
+    <form action="creationCompteClient.php" method="post"> 
+        <label for="nom">Nom* :</label>
         <input type="text" name="nom" id="nom">
-        <br><br>
 
+        <label for="prenom">Prenom* :</label>
+        <input type="text" name="prenom" id="prenom">
+
+        <label for="telephone">Telephone* :</label>
+        <input type="tel" name="telephone" id="telephone">
+
+        <label for="mail">Adresse email* :</label>
+        <input type="mail" name="mail" id="mail">
+
+        <label for="mdp">Mot de passe* :</label>
+        <input type="password" name="mdp" id="mdp">
+        <ul>
+            <li>1 majuscule,</li>
+            <li>1 minuscule,</li>
+            <li>1 chiffre,</li>
+            <li>1 caractère spécial,</li>
+            <li>10 caractère minimum</li>
+        </ul>
+
+        <label for="verifMdp">Vérification du mot de passe* :</label>
+        <input type="password" name="verifMdp" id="verifMdp">
+
+        <p>Vous avez déjà un compte ?</p>
+        <a href='seConnecterCompteClient.php'>Connectez vous</a>   <!-- lien à revoir en fonction du nom du fichier qui permet de se connecter -->
+
+<!--bouton annuler-->
         <input type="Submit" name="submit" id="submit">
     </form>
 
---
+++ php/verificationFormulaire.php
@@ -0,0 +1,37 @@
+<?php
+
+function verifNomPrenom($nom){
+    if (strlen($nom) > 100 || !preg_match("/^[A-Za-z -]+$/", $nom)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function verifTelephone($tel){
+    if (!preg_match("/^+33[0-9]{9}$/", $tel)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function verifMail($mail){
+    if (strlen($mail) > 150 || !preg_match("/^[A-Za-z]@[A-Za-z].[A-Za-z]$/", $mail)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function verifMotDePasse($mdp){
+    if (!preg_match("/^[A-Za-z0-9@-_#$/.!?%*-+:;,&~|^àçéïîëêäâüûöô]{10,}$/", $mdp)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
