f383a808bf3be28baa3c079a19e69036e81a7ea8
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-04 10:45:23 +0100

    HTML ok sans header et footer

--- html/creationProduit.php
+++ html/creationProduit.php
@@ -12,22 +12,15 @@
         <form action="" method="post">
             <select name="categorie" id="categorie" required>Categories</select>
             <input type="file" name="photo" id="photo" required>
+
             <label for="nom">Nom produit *:</label>
-            <input type="text" name="nom" id="nom">
+            <input type="text" name="nom" id="nom" required>
 
             <label for="nom">Prix *:</label>
-            <input type="text" name="prix" id="prix">
+            <input type="number" name="prix" id="prix" required>
 
-            <label for="nom">Remise :</label>
-            <input type="text" name="remise" id="remise">
-
-            <label for="">Quantité en stock :</label>
-            <input type="button" value="-">
-            <label for=""></label>
-            <input type="button" value="+">
-
-            <label for="appliquerRemise">Appliquer la remise</label>
-            <input type="checkbox" name="appliquerRemise" id="appliquerRemise">
+            <label for="qteStock">Quantité en stock :</label>
+            <input type="number" name="qteStock" id="qteStock" required>
 
             <label for="mettreEnLigne">Mettre en ligne</label>
             <input type="checkbox" name="mettreEnLigne" id="mettreEnLigne">
@@ -35,10 +28,9 @@
             <label for="mettreEnPromotion">Mettre en promotion</label>
             <input type="checkbox" name="mettreEnPromotion" id="mettreEnPromotion">
 
-            <label for="tags">Tags *:</label>
 
             <label for="nom">Description *:</label>
-            <input type="text" name="remise" id="remise">
+            <input type="text" name="description" id="description" required>
             
             <input type="button" value="Retour" href="">
             <input type="submit" value="Valider">
@@ -47,3 +39,9 @@
     <footer></footer>
 </body>
 </html>
+
+<?php
+
+
+
+?>
