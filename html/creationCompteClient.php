fcb2ecbdf79614933071aad4669697844d1cb7df
Diwezha <diwezha.ruveron@gmail.com>
2025-11-04 09:20:36 +0100

    Création du fichier du formulaire de création d'un compte client

---
+++ html/creationCompteClient.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Création du compte client</h2>
+
+    <form action=".php" method="post">
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" id="nom">
+        <br><br>
+
+        <input type="Submit" name="submit" id="submit">
+    </form>
+
+</body>
+</html>
