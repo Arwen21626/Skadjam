2b39ccce80447bbaf6dfc7ad90ca24f230670825
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-04 11:01:48 +0100

    Creation des variables php pour l'insertion (pas l'image)

--- html/creationProduit.php
+++ html/creationProduit.php
@@ -10,17 +10,21 @@
     <main>
         <h2>Création d'un produit</h2>
         <form action="" method="post">
-            <select name="categorie" id="categorie" required>Categories</select>
+            <select name="categorie" id="categorie" required>
+                <option value="0">Categorie</option>
+                <option value="id dans la bdd">Boucle php sur la bdd</option>
+            </select>
+            
             <input type="file" name="photo" id="photo" required>
 
             <label for="nom">Nom produit *:</label>
             <input type="text" name="nom" id="nom" required>
 
-            <label for="nom">Prix *:</label>
-            <input type="number" name="prix" id="prix" required>
+            <label for="prix">Prix *:</label>
+            <input type="number" name="prix" id="prix" min="0.0" step="0.5" required>
 
             <label for="qteStock">Quantité en stock :</label>
-            <input type="number" name="qteStock" id="qteStock" required>
+            <input type="number" name="qteStock" id="qteStock" min="0" required>
 
             <label for="mettreEnLigne">Mettre en ligne</label>
             <input type="checkbox" name="mettreEnLigne" id="mettreEnLigne">
@@ -41,6 +45,14 @@
 </html>
 
 <?php
+
+$categorie = htmlentities($_POST['categorie']);
+$nom = htmlentities($_POST['nom']);
+$prix = htmlentities($_POST['prix']);
+$qteStock = htmlentities($_POST['qteStock']);
+$enLigne = htmlentities($_POST['mettreEnLigne']);
+$enPromotion = htmlentities($_POST['mettreEnPromotion']);
+$description = $enLigne = htmlentities($_POST['description']);
 
 
 
