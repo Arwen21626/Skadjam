cac91353f7a6423926b8901dbb0839f419dfb25b
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-04 10:37:29 +0100

    <button> changé en <form>
    <form><input></form> est plus simple à utiliser en php que <button>

--- html/page_client.php
+++ html/page_client.php
@@ -3,20 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/general_front.css">
     <title>Mon compte</title>
 </head>
 <body>
-    
+    <?php
+        
+    ?>
     <h2>Mon compte</h2>
     <article>
         <section>
+            <img src="<?php echo $photo ?>" alt="Photo de profil" width="50" height="50">
             <h3><?php echo $nom ?></h3>
             <h3><?php echo $prenom ?></h3>
         </section>
         <h3><?php echo $telephone ?></h3>
         <h3><?php echo $mail ?></h3>
     </article>
-    <button>Modifier</button>
-    <button>Se déconnecter</button>
+    <form action="modifier_client.php"><input type="submit" value="Modifier"></form>
+    <form action="index.php"><input type="submit" value="Se déconnecter"></form>
 </body>
 </html>
