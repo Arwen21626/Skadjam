4ccfbf398c07967c332f6b7f7da02698c803e441
Julien <julien.lefrancois20@gmail.com>
2025-11-05 14:09:03 +0100

    maj bdd

--- html/01_premiere_connexion.php
+++ html/01_premiere_connexion.php
@@ -3,7 +3,7 @@ include('connect_params.php');
 try {
     $dbh = new PDO("$driver:host=$server;dbname=$dbname", 
             $user, $pass);
-    foreach($dbh->query('SELECT * from _compte', 
+    foreach($dbh->query('SELECT * from sae3_skadjam._compte', 
                         PDO::FETCH_ASSOC) 
                 as $row) {
         echo "<pre>";
