fbad0ae989f5d91373262a346fd3866a3d5e717c
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-07 10:16:18 +0100

    Maj Structure + CSS
    Début de la structure front pour le header & footer + css

---
+++ html/php/header_front.php
@@ -0,0 +1,44 @@
+<header>
+    <section id="header-section">
+        <img src="../images/logo/alizon/logo_alizon_vert_fonce.webp" alt="logo Alizon" class="logo">
+        <h1>Alizon</h1>
+    </section>
+        
+    <nav id="nav-section">
+        <section>
+            <div class="logo_accueil logo_nav_div">
+                <a href="#A modifier">
+                    <div class="logo_nav_img"></div>
+                </a>
+
+                <a href="#A modifier">Accueil</a>
+            </div>
+            
+            <div class="logo_commandes logo_nav_div">
+                <a href="#A modifier">
+                    <div class="logo_nav_img"></div>
+                </a>
+
+                <a href="#A modifier">Mes commandes</a>
+            </div>
+            
+            <div class="logo_creer_produit logo_nav_div">
+                <a href="#A modifier">
+                    <div class="logo_nav_img"></div>
+                </a>
+
+                <a href="#A modifier">Créer un produit</a>
+            </div>
+            
+            <!-- A changer plus tard en menu burger avec la dernière icône -->
+            <div class="logo_profil logo_nav_div">
+                <a href="#A modifier">
+                    <div class="logo_nav_img"></div>
+                </a>
+
+                <a href="#A modifier">Mon Profil</a>
+            </div>
+            
+        </section>
+    </nav>
+</header>
