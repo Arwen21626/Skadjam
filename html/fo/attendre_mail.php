d74268480f2687db166d0109a82e82cf4a9732a8
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-07 09:06:29 +0100

    CSS general-front ajouté

--- html/fo/attendre_mail.php
+++ html/fo/attendre_mail.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bo/general_front.css">
     <title>Réinitialiser mon mot de passe</title>
 </head>
 <body>
