b2844f584e6f2300b5407d33f8aaa02a9d29b7d6
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-07 13:15:46 +0100

    Mise en commentaire de lignes dans 01_premiere_connexion.php

--- html/01_premiere_connexion.php
+++ html/01_premiere_connexion.php
@@ -2,18 +2,18 @@
 
 // Julien : C'est pour mes test, tout marche nickel
 
-echo __DIR__;
+// echo __DIR__;
 include './connections_params.php';
 try {
     $dbh = new PDO("$driver:host=$server;dbname=$dbname", 
             $user, $pass);
-    foreach($dbh->query('SELECT * from sae3_skadjam._vendeur', PDO::FETCH_ASSOC) as $row) {
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
-        echo $row['id_compte'];
-    }
-    $dbh = null;
+    // foreach($dbh->query('SELECT * from sae3_skadjam._vendeur', PDO::FETCH_ASSOC) as $row) {
+    //     echo "<pre>";
+    //     print_r($row);
+    //     echo "</pre>";
+    //     echo $row['id_compte'];
+    // }
+    // $dbh = null;
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
