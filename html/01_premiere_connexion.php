89ec39fecd7306176a7c10c47bb3d45c6af27682
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-07 10:04:51 +0100

    mise a plat pour le merge

--- html/01_premiere_connexion.php
+++ html/01_premiere_connexion.php
@@ -4,15 +4,14 @@ include './connections_params.php';
 try {
     $dbh = new PDO("$driver:host=$server;dbname=$dbname", 
             $user, $pass);
-    /*
-    foreach($dbh->query('SELECT * from sae3_skadjam._compte', 
-                        PDO::FETCH_ASSOC) 
-                as $row) {
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
-    }
-        */
+    // foreach($dbh->query('SELECT * from sae3_skadjam._compte', 
+    //                     PDO::FETCH_ASSOC) 
+    //             as $row) {
+    //     echo "<pre>";
+    //     print_r($row);
+    //     echo "</pre>";
+    // }
+        
     // $dbh = null;
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
