a71f45c9333424009a3e12a64378a6489158e9ae
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-07 09:20:08 +0100

    Ajout des link css

--- html/fo/attendre_mail.php
+++ html/fo/attendre_mail.php
@@ -1,10 +1,9 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bo/general_front.css">
+    <link rel="stylesheet" type="text/css" href="../css/bo/general_front.css">
     <title>Réinitialiser mon mot de passe</title>
 </head>
 <body>
--- html/fo/nouveau_mdp.php
+++ html/fo/nouveau_mdp.php
@@ -7,7 +7,7 @@ require_once "../php/verification_formulaire.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bo/general_front.css">
+    <link rel="stylesheet" type="text/css" href="../css/bo/general_front.css">
     <title>Nouveau mot de passe</title>
 </head>
 <body>
