f01672e272ee389f8e20f0ae24f6fe2001d925b4
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-07 11:18:55 +0100

    $d river corrigé

--- html/fo/attendre_mail.php
+++ html/fo/attendre_mail.php
@@ -13,8 +13,7 @@
     <p>
     <?php
     try{
-        $dbh = new PDO("$d
-        river:host=$server;dbname=$dbname",$user,$pass);
+        $dbh = new PDO("$driver:host=$server;dbname=$dbname",$user,$pass);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
