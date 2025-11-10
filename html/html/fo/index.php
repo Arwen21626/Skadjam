f57ada833487ea7978a62a66c762f1c66dbc7bae
Anarkyia <s.salvadorhoarau@gmail.com>
2025-11-10 12:10:05 +0100

    changements conditions affichage du nombre d'étoiles d'un produit

--- html/html/fo/index.php
+++ html/html/fo/index.php
@@ -69,59 +69,63 @@
                     <!--affichage du prix du produit-->   
                     <p><?php echo htmlentities($valeurs['prix_ttc']);?></p>
 
-                    <!--affichage de la note-->
-                    <?php $note = $valeurs['note'];
+                    <!--récupération de la note-->
+                    <?php $note = $valeurs['note_moyenne'];
 
-                        //note nulle
+                        //affichage d'une note nulle
                         if ($note == null){ ?>
                             <p><?php echo htmlentities('non noté'); ?></p>
                         <?php } 
-                        else {
 
-                            //note entière 
-                            if(($note == 0) || ($note == 1) || ($note == 2) || ($note == 3) || ($note == 4) || ($note == 5)){
-                                $cinqMoinsNote = 5-$note;
-                                //boucle pour étoiles pleines
-                                for($i=0; $i<$note; $i++){?>
-                                    <img src="../../images/logo/bootstrap_icon/star-fill.svg" alt="étoile pleine">
-                                <?php }
-                                //boucle pour étoiles vides
-                                for($i=0; $i<$cinqMoinsNote; $i++){?>
-                                    <img src="../../images/logo/bootstrap_icon/star.svg" alt="étoile vide">
-                                <?php }
+                        else {
+                            $entierPrec = intval($note);
+                            $entierSuiv = $entierPrec+1;
+                            $moitie = $entierPrec+0.5;
+                            $noteFinale;
+                            $nbEtoilesVides;
+
+                            //note arrondie à l'entier précédent
+                            if($note < $entierPrec+0.3){
+                                $noteFinale = $entierPrec;
                             }
 
-                            //note à virgule
-                            else{
-                                $entierPrec = intval($note);
-                                $entierSuiv = $entierPrec+1;
-                                $moitie = $entierPrec+0.5;
-                                $noteFinale;
-                                if($note < $entierPrec+0.3){
-                                    $noteFinale = $entierPrec;
-                                }
-                                else if(($note < $moitie) || ($note < $entierPrec+0.8)){
-                                    $noteFinale = $moitie;
-                                }
-                                else{
-                                    $noteFinale = $entierSuiv;
-                                }
-                                $cinqMoinsNote = 5-$partieEntiere-1;
+                            //note arrondie à 0.5
+                            else if(($note < $moitie) || ($note < $entierPrec+0.8)){
+                                $noteFinale = $moitie;
+                                $nbEtoilesVides = 5-$entierPrec-1;
+                                //affichage d'une note et demie
                                 //boucle pour étoiles pleines
-                                for($i=0; $i<$partieEntiere; $i++){?>
+                                for($i=0; $i<$entierPrec; $i++){?>
                                     <img src="../../images/logo/bootstrap_icon/star-fill.svg" alt="étoile pleine">
                                 <?php } ?>
                                 <!--demie étoile-->
                                 <img src="../../images/logo/bootstrap_icon/star-half.svg" alt="demie étoile">
                                 <!--boucle pour étoiles vides-->
-                                <?php for($i=0; $i<$cinqMoinsNote; $i++){?>
+                                <?php for($i=0; $i<$nbEtoilesVides; $i++){?>
                                     <img src="../../images/logo/bootstrap_icon/star.svg" alt="étoile vide">
                                 <?php }
                             }
-                        }?>                
-            <?php }
+                            
+                            //note arrondie à l'entier suivant
+                            else{
+                                $noteFinale = $entierSuiv;
+                            }
 
-                $dbh = null;
+                            //affichage d'une note entière :
+                            if($noteFinale != $moitie){
+                                $nbEtoilesVides = 5-$noteFinale;
+                                //boucle pour étoiles pleines
+                                for($i=0; $i<$noteFinale; $i++){?>
+                                    <img src="../../images/logo/bootstrap_icon/star-fill.svg" alt="étoile pleine">
+                                <?php }
+                                //boucle pour étoiles vides
+                                for($i=0; $i<$nbEtoilesVides; $i++){?>
+                                    <img src="../../images/logo/bootstrap_icon/star.svg" alt="étoile vide">
+                                <?php }
+                            }
+                        }  
+                }             
+            $dbh = null;
             } 
 
             catch (PDOException $e) {
