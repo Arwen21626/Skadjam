fa2b1819a55ea0f5e35b7f40d3e3557fc2d1d019
Seraphim5012 <mewen.roussel@hotmail.com>
2025-11-10 11:03:56 +0100

    format num & adresse

--- html/php/modification_variable.php
+++ html/php/modification_variable.php
@@ -9,4 +9,21 @@ function format_tel($tel){
     $tel = str_replace(" ", "", trim($tel));
     return "+33". substr($tel, 1);
 }
+
+function num_rue($adresse){
+if (preg_match('/^(\d+\s*[A-Za-z]*)[, ]*(.+)$/u', $adresse, $matches)) {
+    $numero = trim($matches[1]); 
+
+    return $numero;
+}
+
+}
+
+function format_adresse($adresse){
+    if (preg_match('/^(\d+\s*[A-Za-z]*)[, ]*(.+)$/u', $adresse, $matches)) {
+    $rue = trim($matches[2]);
+
+    return $rue;
+}
+}
 ?>
