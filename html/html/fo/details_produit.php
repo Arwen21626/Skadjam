f12cb140feb363acd96beb54cdce8406fb3c9c41
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-10 15:07:59 +0100

    Create details_produit.php
    Création de la page pour les détails d'un produit du côté front - client

---
+++ html/html/fo/details_produit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require("html/php/structure/head_front.php") ?>
+    <title>"Nom du produit"</title>
+</head>
+<body>
+    <?php require("html/php/structure/header_front.php") ?>
+
+    <main></main>
+
+    <?php require("html/php/structure/footer_front.php") ?>
+</body>
+</html>
