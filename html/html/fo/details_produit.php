ca55b9099a4c7ab8d621ed8bbc1666f1e12ade8d
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-12 10:14:33 +0100

    Update details_produit.php

--- html/html/fo/details_produit.php
+++ html/html/fo/details_produit.php
@@ -9,7 +9,7 @@
     <?php require("html/php/structure/header_front.php"); ?>
     <?php require("html/php/structure/navbar_front.php"); ?>
 
-    <main class="bg-vertClair">
+    <main>
         <!-- Section Description -->
         <section>
             <article> <!-- Titrage -->
