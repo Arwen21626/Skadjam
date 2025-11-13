3501b6a659396a3e12e804e0682aac8297f9c128
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-13 15:37:27 +0100

    lien dans la nav bar

--- html/php/structure/navbar_back.php
+++ html/php/structure/navbar_back.php
@@ -5,7 +5,7 @@
                 <div class="logo_nav_img"></div>
             </a>
 
-            <a href="#A modifier">Accueil</a>
+            <a href="/html/html/bo/index_vendeur.php">Accueil</a>
         </div>
 
         <div class="logo_commandes logo_nav_div">
@@ -17,11 +17,11 @@
         </div>
 
         <div class="logo_creer_produit logo_nav_div">
-            <a href="#A modifier">
+            <a href="/html/html/bo/creation_produit.php">
                 <div class="logo_nav_img"></div>
             </a>
 
-            <a href="#A modifier">Créer un produit</a>
+            <a href="/html/html/bo/creation_produit.php">Créer un produit</a>
         </div>
 
         <!-- A changer plus tard en menu burger avec la dernière icône -->
