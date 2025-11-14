bb1fbde69f3de09a94241d181904c32c0747da9c
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-14 14:57:32 +0100

    déconnexion dans page_client

--- html/html/fo/nouveau_mdp.php
+++ html/html/fo/nouveau_mdp.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
-require_once "../php/verification_formulaire.php";
-require_once "../connections_params.php";
+require_once __DIR__ . "/../../php/verification_formulaire.php";
+require_once __DIR__ . "/../../01_premiere_connexion.php";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/fo/general_front.css">
+    <?php require __DIR__ . "/../../php/structure/head_front.php"; ?>
     <title>Nouveau mot de passe</title>
 </head>
 <body>
     <?php
+    require __DIR__ . "/../../php/structure/header_front.php";
+
     try{
         $dbh = new PDO("$driver:host=$server;dbname=$dbname",$user,$pass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         // Traitement du formulaire seulement si toutes les données son saisie
         if(isset($_POST['mdp']) && isset($_POST['verifMdp'])){
@@ -28,28 +29,29 @@ require_once "../connections_params.php";
                 echo "Erreur";
             }
         }else{ ?>
-        <h2>Création du compte client</h2>
+            <h2>Nouveau mot de passe</h2>
 
-        <form action="nouveau_mdp.php" method="post"> 
-            <label for="mdp">Mot de passe* :</label>
-            <input type="password" name="mdp" id="mdp" required>
-            <ul>
-                <li>1 majuscule,</li>
-                <li>1 minuscule,</li>
-                <li>1 chiffre,</li>
-                <li>1 caractère spécial,</li>
-                <li>10 caractère minimum</li>
-            </ul>
+            <form action="nouveau_mdp.php" method="post"> 
+                <label for="mdp">Mot de passe* :</label>
+                <input type="password" name="mdp" id="mdp" required>
+                <ul>
+                    <li>1 majuscule,</li>
+                    <li>1 minuscule,</li>
+                    <li>1 chiffre,</li>
+                    <li>1 caractère spécial,</li>
+                    <li>10 caractère minimum</li>
+                </ul>
 
-            <label for="verifMdp">Vérification du mot de passe* :</label>
-            <input type="password" name="verifMdp" id="verifMdp" required>
+                <label for="verifMdp">Vérification du mot de passe* :</label>
+                <input type="password" name="verifMdp" id="verifMdp" required>
 
-
-            <input type="submit" value="Confirmer">
-        </form>
-        <?php }
+                <input type="submit" value="Confirmer">
+            </form><?php
+        }
     }catch(PDOException $e){
         print "Erreur : " . $e->getMessage() . "<br/>";
-    } ?>
+    }
+    require __DIR__ . "/../../php/structure/footer_front.php";
+    ?>
 </body>
 </html>
