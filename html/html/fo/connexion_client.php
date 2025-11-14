b2efd72a2b429d7191f98198b3ce0db34b80ae00
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-14 08:49:33 +0100

    orhtographe toujours

--- html/html/fo/connexion_client.php
+++ html/html/fo/connexion_client.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connection client</title>
+    <title>Connexion client</title>
     
     <?php require_once(__DIR__ . '/../../php/structure/head_front.php');?>
     <style>
