4c5b4a3493bec6b4c1a7bcbf3fcf856dcbadc461
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-15 14:41:28 +0100

    Maj structure et css

--- html/php/structure/header_front.php
+++ html/php/structure/header_front.php
@@ -1,11 +1,11 @@
 <header class="bg-vertClair">
-    <section id="header-section">
-        <div>
+    <section id="header-section" class="flex justify-between pt-2 pr-2 pb-2">
+        <div class="pl-2">
             <a href="#A modifier">
-                <img src="/html/images/logo/alizon/logo_alizon_vert_fonce.webp" alt="logo Alizon" class="logo">
+                <img src="/html/images/logo/alizon/logo_alizon_vert_fonce.webp" alt="logo Alizon" class="size-20">
             </a>
         </div>
         
-        <h1 class="text-vertFonce">Alizon</h1>
+        <h1 class="text-vertFonce m-0 text-right pt-1 pr-2">Alizon</h1>
     </section>
 </header>
--- html/php/structure/navbar_front.php
+++ html/php/structure/navbar_front.php
@@ -1,44 +1,61 @@
 <nav class="bg-beige sticky top-0">
-    <section>
+
+    <section class="flex justify-around items-center p-1">
+
         <div class="logo_accueil logo_nav_div">
             <a href="#A modifier">
-                <div class="logo_nav_img"></div>
+                <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
+                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge" href="#A modifier">Accueil</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Accueil</a>
         </div>
             
         <div class="logo_commandes logo_nav_div">
             <a  href="#A modifier">
-                <div class="logo_nav_img"></div>
+                <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
+                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge" href="#A modifier">Commandes</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Commandes</a>
         </div>
             
         <div class="logo_recherche logo_nav_div">
             <a href="#A modifier">
-                <div class="bg-[url(/html/images/logo/bootstrap_icon/person.svg)]"></div>
+                <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
+                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge" href="#A modifier">Recherche</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Recherche</a>
         </div>
 
         <div class="logo_panier logo_nav_div">
             <a href="#A modifier">
-                <div class="logo_nav_img"></div>
+                <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
+                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge" href="#A modifier">Panier</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Panier</a>
         </div>
             
         <!-- A changer plus tard en menu burger avec la dernière icône -->
         <div class="logo_profil logo_nav_div">
             <a href="#A modifier">
-                <div class="logo_nav_img"></div>
+                <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
+                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge" href="#A modifier">Profil</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Profil</a>
         </div>
             
     </section>
