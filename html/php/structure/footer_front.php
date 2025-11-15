6792003fa47316ce42cd09f0149ea29f69b155ed
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-15 15:37:07 +0100

    maj css structure

--- html/php/structure/footer_front.php
+++ html/php/structure/footer_front.php
@@ -1,39 +1,52 @@
-<footer class="bg-vertClair text-vertFonce">
-    <article>
-        <h3>Infos Pratiques</h3>
-        <a class="hover:text-rouge" href="./cgv_fo.php">
+<footer class="bg-vertClair text-vertFonce 
+                flex flex-wrap p-0.5">
+
+    <article class="flex flex-col max-w-1/2 pt-1 pr-2 pb-2 pl-2">
+        <h3 class="m-2">Infos Pratiques</h3>
+
+        <a class="hover:text-rouge m-1" href="./cgv_fo.php">
             Conditions générales de vente
         </a>
 
-        <a class="hover:text-rouge" href="./cgu_fo.php">
+        <a class="hover:text-rouge m-1" href="./cgu_fo.php">
             Conditions générales d'utilisation
         </a>
     </article>
 
-    <article>
-        <h3>À propos</h3>
-        <a class="hover:text-rouge" href="Insérer lien page info sur la COBREC">Qui sommes-nous ?</a>
-        <a class="hover:text-rouge" href="Insérer lien vers le Site de la COBREC">Site de la COBREC</a>
+    <article class="flex flex-col max-w-1/2 pt-1 pr-2 pb-2 pl-2">
+        <h3 class="m-2">À propos</h3>
+
+        <a class="hover:text-rouge m-1" href="Insérer lien page info sur la COBREC">Qui sommes-nous ?</a>
+        <a class="hover:text-rouge m-1" href="Insérer lien vers le Site de la COBREC">Site de la COBREC</a>
     </article>
 
-    <article>
-        <h3>Nous contacter</h3>
-        <a class="hover:text-rouge" href="mailto:questions@cobrec.bzh">questions@cobrec.bzh</a>
-        <a class="hover:text-rouge" href="tel:0295264512">02 95 26 45 12</a>
+    <article class="flex flex-col max-w-1/2 pt-1 pr-2 pb-2 pl-2">
+        <h3 class="m-2">Nous contacter</h3>
+        <a class="hover:text-rouge m-1" href="mailto:questions@cobrec.bzh">questions@cobrec.bzh</a>
+        <a class="hover:text-rouge m-1" href="tel:0295264512">02 95 26 45 12</a>
     </article>
 
-    <article id="section-reseaux">
-        <a class="hover:text-rouge" href="A modifier" class="reseau_item">
-            <img src="/html/images/logo/reseaux/facebook_front.webp" alt="Facebook" title="Facebook" class="logo_reseaux">
+    <article class="grid grid-cols-2 gap-0.5">
+
+        <a class="hover:text-rouge m-1" href="A modifier" class="self-center">
+
+            <img src="/html/images/logo/reseaux/facebook_front.webp" alt="Facebook" title="Facebook" 
+            class="size-12">
         </a>
-        <a class="hover:text-rouge" href="A modifier" class="reseau_item">
-            <img src="/html/images/logo/reseaux/instagram_front.webp" alt="Instagram" title="Instagram" class="logo_reseaux">
+        <a class="hover:text-rouge m-1" href="A modifier" class="self-center">
+
+            <img src="/html/images/logo/reseaux/instagram_front.webp" alt="Instagram" title="Instagram" 
+            class="size-12">
         </a>
-        <a class="hover:text-rouge" href="A modifier" class="reseau_item">
-            <img src="/html/images/logo/reseaux/linkedin_front.webp" alt="Linkedin" title="Linkedin" class="logo_reseaux">
+        <a class="hover:text-rouge m-1" href="A modifier" class="self-center">
+
+            <img src="/html/images/logo/reseaux/linkedin_front.webp" alt="Linkedin" title="Linkedin" 
+            class="size-12">
         </a>
-        <a class="hover:text-rouge" href="A modifier" class="reseau_item">
-            <img src="/html/images/logo/reseaux/twitter_x_front.webp" alt="Twitter - X" title="Twitter - X" class="logo_reseaux">
+        <a class="hover:text-rouge m-1" href="A modifier" class="self-center">
+            
+            <img src="/html/images/logo/reseaux/twitter_x_front.webp" alt="Twitter - X" title="Twitter - X" 
+            class="size-12">
         </a>
     </article>  
 </footer>
--- html/php/structure/navbar_front.php
+++ html/php/structure/navbar_front.php
@@ -2,60 +2,60 @@
 
     <section class="flex justify-around items-center p-1">
 
-        <div class="logo_accueil logo_nav_div">
-            <a href="#A modifier">
+        <div class="">
+            <a class="items-center" href="#A modifier">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
                 bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
                 hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Accueil</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline" href="#A modifier">Accueil</a>
         </div>
             
         <div class="logo_commandes logo_nav_div">
             <a  href="#A modifier">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
-                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
-                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                bg-[url(/html/images/logo/bootstrap_icon/truck.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/truck-fill.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Commandes</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline" href="#A modifier">Commandes</a>
         </div>
             
         <div class="logo_recherche logo_nav_div">
             <a href="#A modifier">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
-                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
-                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                bg-[url(/html/images/logo/bootstrap_icon/search.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/search-selected.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Recherche</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline" href="#A modifier">Recherche</a>
         </div>
 
         <div class="logo_panier logo_nav_div">
             <a href="#A modifier">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
-                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
-                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                bg-[url(/html/images/logo/bootstrap_icon/cart.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/cart-fill.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Panier</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline" href="#A modifier">Panier</a>
         </div>
             
         <!-- A changer plus tard en menu burger avec la dernière icône -->
         <div class="logo_profil logo_nav_div">
             <a href="#A modifier">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
-                bg-[url(/html/images/logo/bootstrap_icon/house.svg)] 
-                hover:bg-[url(/html/images/logo/bootstrap_icon/house-fill.svg)]">
+                bg-[url(/html/images/logo/bootstrap_icon/person.svg)] 
+                hover:bg-[url(/html/images/logo/bootstrap_icon/person-fill.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:contents" href="#A modifier">Profil</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline" href="#A modifier">Profil</a>
         </div>
             
     </section>
