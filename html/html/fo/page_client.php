ff48f4efcde3c0485e473df7c0dde45b26cc07a8
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-17 10:00:54 +0100

    Divers correction sur les pages clients

--- html/html/fo/page_client.php
+++ html/html/fo/page_client.php
@@ -32,7 +32,7 @@
             $id = (int) $_SESSION["idCompte"];
 
             try{
-                $dbh = new PDO("$driver:host=$server;dbname=$dbname",$user,$pass);
+                $dbh = new PDO("$driver:host=$server;port=$port;dbname=$dbname",$user,$pass);
                 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
