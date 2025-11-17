8b9859b98aed9be2d6a300a7c5f6a7807ed0449a
Julien <julien.lefrancois20@gmail.com>
2025-11-17 14:04:22 +0100

    maj connexion

--- html/html/fo/connexion.php
+++ html/html/fo/connexion.php
@@ -1,56 +1,76 @@
 <?php 
 session_start();
 include __DIR__ . '/../../01_premiere_connexion.php';
-if (isset($_POST["mail"])){
+if(isset($_POST['mdp']) && isset($_POST['mail'])){
     $mail = $_POST["mail"];
     $mdp = $_POST["mdp"];
-    $stmt = $dbh->prepare("SELECT c.motdepasse, c.id_compte FROM sae3_skadjam._compte c inner join sae3_skadjam._vendeur v on c.id_compte = v.id_compte where adresse_mail = ?");
+    //echo 'mail : '.$mail;
+    //echo 'mdp : '.$mdp;
+    $stmt = $dbh->prepare("SELECT * FROM sae3_skadjam._compte WHERE adresse_mail = ?");
     $stmt->execute([$mail]);
-    $passHash = $stmt->fetchColumn(0);
-    $idCompte = $stmt->fetchColumn(1);
-    $passCorrect = password_verify($mdp, $passHash);
+    $tab = $stmt->fetch(PDO::FETCH_ASSOC);
+    $mdpHasher = password_hash($tab['mot_de_pass'], PASSWORD_DEFAULT);
+    //echo '<pre>';
+    //$passCorrect = $mdp == $tab['mot_de_passe'];
+    $passCorrect = password_verify($tab['mot_de_passe'], $mdpHasher);
+    echo $mdpHasher;
+    //print_r($_SESSION);
+    //print_r($tab);
+    
+    //echo '<pre>';
 
     if ($passCorrect){
-        header("location:index_vendeur.php");
+        $_SESSION['id_compte'] = $tab['id_compte'];
+        //header('Location: /../index.php');
+        //echo 'correct';
+    }else{
+        //echo 'return';
     }
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once __DIR__ . "/../../php/structure/head_back.php"?>
+<?php require_once __DIR__ . "/../../php/structure/head_front.php"?>
 <head>
     
-    <title>connexion vendeur</title>
+    <title>connexion</title>
 </head>
 <body>
-    <?php require_once __DIR__ . "/../../php/structure/header_back.php"; ?>
+    <?php require_once __DIR__ . "/../../php/structure/header_front.php"; ?>
     <main>
         <h2>Connexion</h2>
-        <form method="POST">
+        <form method="post">
             <div class="flex flex-col items-center ml-10 mb-7 mr-10">
 
                 <div class="flex w-fit flex-col items-start">
                     <label for="mail">Adresse mail :</label>
-                    <input class="ml-5 border-5 border-solid rounded-2xl border-bleu pl-3 mb-5" type="text" name="mail" id="mail" required>
+                    <input class="ml-5 border-5 border-solid rounded-2xl border-vertClair pl-3 mb-5 w-150 h-15" type="text" name="mail" id="mail" required>
                 </div>
 
                 <div class="flex w-fit flex-col mt-6 items-start">
                     <label for="mdp">Mot de passe :</label>
-                    <input class="ml-5 border-5 border-solid rounded-2xl border-bleu pl-3" type="password" name="mdp" id="mdp" required>
+                    <input class="ml-5 border-5 border-solid rounded-2xl border-vertClair pl-3 w-150 h-15" type="password" name="mdp" id="mdp" required>
                     <a href=""></a>
-                    <p class="underline! self-end cursor-pointer">mot de passe oublié ?</p>
+                    <br>
+                    <a href="nouveau_mdp.php" class="underline! self-end cursor-pointer">mot de passe oublié ?</a>
                 </div class="flex w-fit flex-col mt-6 items-start">
 
                 <div class=" justify-self-center mt-8 mb-8">
-                    <input type="submit" value="Se connecter" class="cursor-pointer w-64 border-5 border-solid rounded-2xl border-bleu pl-3">
+                    <input type="submit" value="Se connecter" class="cursor-pointer w-64 border-5 border-solid rounded-2xl border-vertClair pl-3">
                 </div>
             </div>
         </form>
         <div class="flex flex-row flex-wrap justify-center m-2">
+            <p class=" mr-2">Pas encore client ? </p>
+            <a href=".crea_compte_client.php" class="underline!">Créer un compte client</a>
+        </div>
+        <div class="flex flex-row flex-wrap justify-center m-2">
             <p class=" mr-2">Pas encore vendeur ? </p>
-            <a href="crea_compte_vendeur.php" class="underline!">Créer un compte</a>
+            <a href="../bo/crea_compte_vendeur.php" class="underline!">Créer un compte vendeur</a>
         </div>
     </main>
+    <?php require_once __DIR__ . "/../../php/structure/footer_front.php"; ?>
 </body>
 </html>
