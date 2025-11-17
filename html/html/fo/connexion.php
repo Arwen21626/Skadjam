ad8003bcb312ad337f7ea410f5fbbb2af80a8673
Julien <julien.lefrancois20@gmail.com>
2025-11-17 16:36:14 +0100

    maj

--- html/html/fo/connexion.php
+++ html/html/fo/connexion.php
@@ -1,9 +1,10 @@
 <?php 
 session_start();
+$erreur = false;
 include __DIR__ . '/../../01_premiere_connexion.php';
 if(isset($_POST['mdp']) && isset($_POST['mail'])){
     // Initialisation des données
-    $erreur = false;
+    
     $mail = htmlentities($_POST["mail"]);
     $mdp = htmlentities($_POST["mdp"]);
 
