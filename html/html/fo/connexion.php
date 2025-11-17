951fab9a9d47a4844dcaf0d0a1eb8f3daa73ae17
Julien <julien.lefrancois20@gmail.com>
2025-11-17 16:30:11 +0100

    page connexion fini + DOD fait

--- html/html/fo/connexion.php
+++ html/html/fo/connexion.php
@@ -2,26 +2,40 @@
 session_start();
 include __DIR__ . '/../../01_premiere_connexion.php';
 if(isset($_POST['mdp']) && isset($_POST['mail'])){
+    // Initialisation des données
     $erreur = false;
-    $mail = $_POST["mail"];
-    $mdp = $_POST["mdp"];
-    //echo 'mail : '.$mail;
-    //echo 'mdp : '.$mdp;
-    $stmt = $dbh->prepare("SELECT * FROM sae3_skadjam._compte WHERE adresse_mail = ?");
+    $mail = htmlentities($_POST["mail"]);
+    $mdp = htmlentities($_POST["mdp"]);
+
+    // Récupération des données de la bdd pour tester la connexion
+    $stmt = $dbh->prepare("SELECT id_compte, mot_de_passe FROM sae3_skadjam._compte WHERE adresse_mail = ?");
     $stmt->execute([$mail]);
     $tab = $stmt->fetch(PDO::FETCH_ASSOC);
-    //echo '<pre>';
+
+    // Vérification mot de passe
     $passCorrect = password_verify($mdp, $tab['mot_de_passe']);
-    //print_r($_SESSION);
-    //print_r($tab);
     
-    //echo '<pre>';
 
     if ($passCorrect){
+        // Initialisation de la session après confirmation du mot de passe
         $_SESSION['id_compte'] = $tab['id_compte'];
-        header('Location: /../index.php');
+
+        // Récupération des données de la bdd pour voir si c'est un vendeur ou un client
+        $stmt = $dbh->prepare("SELECT id_compte FROM sae3_skadjam._vendeur WHERE id_compte = ?");
+        $stmt->execute([$tab['id_compte']]);
+        $estVendeur = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($estVendeur['id_compte']){ 
+            // Vendeur
+            header('Location: ../bo/index_vendeur.php');
+        }else{ 
+            // Client
+            header('Location: ../fo/index.php');
+        }
+        
     }
     else{
+        // Erreur détecté dans l'adresse mail ou le mot de passe
         $erreur = true;
     }
 }
@@ -52,25 +66,30 @@ if(isset($_POST['mdp']) && isset($_POST['mail'])){
                     <input class="ml-5 border-5 border-solid rounded-2xl border-vertClair pl-3 w-150 h-15" type="password" name="mdp" id="mdp" required>
                     <a href=""></a>
                     <br>
-                    <a href="nouveau_mdp.php" class="underline! self-end cursor-pointer hover:text-rouge">mot de passe oublié ?</a>
+                    <!-- Renvoie sur la page de réinitialisation de mot de passe -->
+                    <a href="reinitialiser_mdp.php" class="underline! self-end cursor-pointer hover:text-rouge">mot de passe oublié ?</a>
                 </div class="flex w-fit flex-col mt-6 items-start">
 
                 <div class=" justify-self-center mt-8 mb-8">
+                    <!-- Envoie des données en méthode POST pour se connecter -->
                     <input type="submit" value="Se connecter" class="cursor-pointer w-64 border-5 border-solid rounded-2xl border-vertClair pl-3">
                 </div>
                 
                 <div class=" flex w-fit flex-col mt-6 items-start ">
+                    <!-- Si erreur détecté -->
                     <?php if($erreur){ ?>
-                        <p class="text-rouge"><?php echo 'adresse mail ou mot de passe invalide !';?></p>
-                    <?php } ?>
+                        <p class="text-rouge items-center"><?php echo 'adresse mail ou mot de passe invalide';?></p>
+                    <?php }?>
                 </div>
                 
             </div>
         </form>
+        <!-- Renvoie sur la page de création d'un compte client -->
         <div class="flex flex-row flex-wrap justify-center m-2">
             <p class=" mr-2">Pas encore client ? </p>
-            <a href=".crea_compte_client.php" class="underline! hover:text-rouge">Créer un compte client</a>
+            <a href="./creation_compte_client.php" class="underline! hover:text-rouge">Créer un compte client</a>
         </div>
+        <!-- Renvoie sur la page de création d'un compte vendeur -->
         <div class="flex flex-row flex-wrap justify-center m-2">
             <p class=" mr-2">Pas encore vendeur ? </p>
             <a href="../bo/crea_compte_vendeur.php" class="underline! hover:text-rouge">Créer un compte vendeur</a>
