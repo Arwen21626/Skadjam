2b7563517696ab2d9fb5ddda5cf44ec371d80e35
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2025-11-18 14:43:06 +0100

    reinitialiser_mdp terminé

--- html/html/fo/profil_client.php
+++ html/html/fo/profil_client.php
@@ -73,44 +73,46 @@ if (isset($_POST['logout'])) {
             }catch(PDOException $e){
                 echo "Erreur : " . $e->getMessage();
             }
-        ?>
-        <h2 class="flex justify-center text-center">Mon profil</h2>
-        <section>
-            <!-- Affichage des informations du client (sauf son mot de passe) -->
-            <div class="flex flex-row">
-                <h2 class="mt-1 mb-1 mr-4 ml-0"><?php echo $pseudo; ?></h2>
-                <h3 class="mt-1 mb-1 mr-4 ml-0 relative top-4 text-vertFonce -z-1"><?php echo $prenom; ?> <?php echo $nom; ?></h3>
-            </div>
-            <div>
-                <p class="m-4"><?php echo $naissance; ?></p>
-                <?php for ($i=0; $i < $nbAdresse; $i++) { // Affiche toutes les adresses du client ?>
-                <p class="mt-2 mb-2 mr-4 ml-4"><?php echo "$numRue[$i] $adressePostale[$i]$batiment[$i]$appartement[$i], $codePostal[$i] $ville[$i]"; ?></p>
-                <?php } ?>
-                <p class="m-4"><?php echo $telephone; ?></p>
-                <p class="m-4"><?php echo $mail; ?></p>
-            </div>
-        </section>
-        <article class="flex flex-row justify-around mb-5">
-            <!-- Modifier les informations du client (sauf le mot de passe) -->
-            <form action="modifier_compte_client.php" method="post">
-                <input class="border-2 border-vertClair rounded-xl p-2" type="submit" value="Modifier mes informations">
-            </form>
+            ?>
+            <h2 class="flex justify-center text-center">Mon profil</h2>
+            <section>
+                <!-- Affichage des informations du client (sauf son mot de passe) -->
+                <div class="flex flex-row">
+                    <h2 class="mt-1 mb-1 mr-4 ml-0"><?php echo $pseudo; ?></h2>
+                    <h3 class="mt-1 mb-1 mr-4 ml-0 relative top-4 text-vertFonce -z-1"><?php echo $prenom; ?> <?php echo $nom; ?></h3>
+                </div>
+                <div>
+                    <p class="m-4"><?php echo $naissance; ?></p>
+                    <?php for ($i=0; $i < $nbAdresse; $i++) { // Affiche toutes les adresses du client ?>
+                        <p class="mt-2 mb-2 mr-4 ml-4"><?php echo "$numRue[$i] $adressePostale[$i]$batiment[$i]$appartement[$i], $codePostal[$i] $ville[$i]"; ?></p>
+                    <?php } ?>
+                    <p class="m-4"><?php echo $telephone; ?></p>
+                    <p class="m-4"><?php echo $mail; ?></p>
+                </div>
+            </section>
+            <article class="flex flex-row justify-around mb-5">
+                <!-- Modifier les informations du client (sauf le mot de passe) -->
+                <form action="modifier_compte_client.php" method="post">
+                    <input class="border-2 border-vertClair rounded-xl p-2" type="submit" value="Modifier mes informations">
+                </form>
 
-            <!-- Modifier le mot de passe du client -->
-            <form action="nouveau_mdp.php">
-                <input class="border-2 border-vertClair rounded-xl p-2" type="submit" value="Modifier mon mot de passe">    
-            </form>
+                <!-- Modifier le mot de passe du client -->
+                <form action="nouveau_mdp.php">
+                    <input class="border-2 border-vertClair rounded-xl p-2" type="submit" value="Modifier mon mot de passe">    
+                </form>
 
-            <!-- Déconnexion -->
-            <form action="profil_client.php" method="post">
-                <input type="hidden" id="logout" name="logout" value="true">
-                <input class="border-2 border-vertClair rounded-xl p-2" type="submit" value="Se déconnecter">
-            </form>
-        </article>
-        <?php }else{
+                <!-- Déconnexion -->
+                <form action="profil_client.php" method="post">
+                    <input type="hidden" id="logout" name="logout" value="true">
+                    <input class="border-2 border-vertClair rounded-xl p-2" type="submit" value="Se déconnecter">
+                </form>
+            </article>
+        <?php
+        }else{
             // Si non connecté, l'emmener à la page de connexion à la place
             header("Location: connexion.php");
-        } ?>
+        }
+        ?>
     </main>
     <?php require __DIR__ . "/../../php/structure/footer_front.php"; ?>
 </body>
