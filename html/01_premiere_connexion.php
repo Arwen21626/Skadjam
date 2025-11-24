2fc69bdd99080950a29a679946b579c5affec1a5
Arwen21626 <jobit-b.arwen21@bbox.fr>
2025-11-24 09:44:02 +0100

    mis au propre

--- html/01_premiere_connexion.php
+++ html/01_premiere_connexion.php
@@ -1,8 +1,5 @@
 <?php
 
-// Julien : C'est pour mes test, tout marche nickel
-
-//echo __DIR__;
 include (__DIR__ . '/../connections_params.php');
 try {
     $dbh = new PDO("$driver:host=$server;port=$port;dbname=$dbname", $user, $pass);
