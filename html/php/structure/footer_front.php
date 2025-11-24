edf64f963d0170f8f9de3062c64fe11a106228d2
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-24 11:58:14 +0100

    Update footer_front.php
    ajout des liens réseaux

--- html/php/structure/footer_front.php
+++ html/php/structure/footer_front.php
@@ -29,12 +29,12 @@
 
     <article class="grid grid-cols-2 gap-0.5 md:max-w-fit md:pt-0.5 md:pr-1 md:pb-1 md:pl-1 md:gap-1">
 
-        <a class="hover:text-rouge m-1" href="#A modifier" class="self-center">
+        <a class="hover:text-rouge m-1" href="https://www.facebook.com" class="self-center">
 
             <img src="/images/logo/reseaux/facebook_front.webp" alt="Facebook" title="Facebook" 
             class="size-12 md:size-14">
         </a>
-        <a class="hover:text-rouge m-1" href="#A modifier" class="self-center">
+        <a class="hover:text-rouge m-1" href="https://www.instagram.com" class="self-center">
 
             <img src="/images/logo/reseaux/instagram_front.webp" alt="Instagram" title="Instagram" 
             class="size-12 md:size-14">
@@ -44,7 +44,7 @@
             <img src="/images/logo/reseaux/linkedin_front.webp" alt="Linkedin" title="Linkedin" 
             class="size-12 md:size-14">
         </a>
-        <a class="hover:text-rouge m-1" href="#A modifier" class="self-center">
+        <a class="hover:text-rouge m-1" href="https://x.com/" class="self-center">
             
             <img src="/images/logo/reseaux/twitter_x_front.webp" alt="Twitter - X" title="Twitter - X" 
             class="size-12 md:size-14">
