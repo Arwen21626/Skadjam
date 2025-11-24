afb0a8d85899fdf90d8e55798cae25998eabe2cc
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-24 13:29:44 +0100

    test favicon si ça explose c normal (Hoopa)

--- html/php/structure/head_front.php
+++ html/php/structure/head_front.php
@@ -2,4 +2,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/output.css">
+    <link rel="icon" type="image/webp" href="/images/logo/alizon/logo_alizon_vert_fonce.webp">
 </head>
