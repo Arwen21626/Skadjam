3ce1db1e85a1c1fc48a34a065238d7623b030aaf
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-24 15:13:26 +0100

    Update nav bar

--- html/php/structure/navbar_front.php
+++ html/php/structure/navbar_front.php
@@ -38,26 +38,26 @@
             
         <div class="md:flex md:flex-col md:justify-center md:items-center">
 
-            <a  href="#A modifier">
+            <a  href="/html/fo/commandes.php">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
                 bg-[url(/images/logo/bootstrap_icon/truck.svg)] 
                 hover:bg-[url(/images/logo/bootstrap_icon/truck-fill.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:inline-block" href="#A modifier">Commandes</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline-block" href="/html/fo/commandes.php">Commandes</a>
         </div>
             
         <div class="md:flex md:flex-col md:justify-center md:items-center">
 
-            <a href="#A modifier">
+            <a href="/html/fo/recherche.php">
                 <div class="size-12 bg-no-repeat bg-size-[auto_48px] 
                 bg-[url(/images/logo/bootstrap_icon/search.svg)] 
                 hover:bg-[url(/images/logo/bootstrap_icon/search-selected.svg)]">
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:inline-block" href="#A modifier">Recherche</a>
+            <a class="text-vertFonce hover:text-rouge hidden md:inline-block" href="/html/fo/recherche.php">Recherche</a>
         </div>
 
         <div class="md:flex md:flex-col md:justify-center md:items-center">
