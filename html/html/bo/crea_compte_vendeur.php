24066d03f4e038717f62d4fe1f41a42fd8bc1ba9
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2025-11-24 08:41:03 +0100

    maj correctif details & crea compte vendeur

--- html/html/bo/crea_compte_vendeur.php
+++ html/html/bo/crea_compte_vendeur.php
@@ -130,14 +130,14 @@ if (isset($_POST["nom"])){
 
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once __DIR__ . "/../../php/structure/head_back.php"?>
+<?php require_once __DIR__ . "/../../php/structure/head_front.php"?>
 <head>
     <title>Créer un compte vendeur</title>
 </head>
 <body class=" @container">
     <?php 
-    require_once __DIR__ . "/../../php/structure/header_back.php";
-    require_once __DIR__ . "/../../php/structure/navbar_back.php";
+    require_once __DIR__ . "/../../php/structure/header_front.php";
+    require_once __DIR__ . "/../../php/structure/navbar_front.php";
     ?>
     <main class=" flex flex-col items-center">
         <h2 class="m-8">Inscription Vendeur</h2>
@@ -249,6 +249,6 @@ if (isset($_POST["nom"])){
             <a href="./connexion_compte_vendeur.php" class="underline!" >Connectez vous</a>
         </div>
     </main>
-    <?php require_once __DIR__ . "/../../php/structure/footer_back.php" ?>
+    <?php require_once __DIR__ . "/../../php/structure/footer_front.php" ?>
 </body>
 </html>
