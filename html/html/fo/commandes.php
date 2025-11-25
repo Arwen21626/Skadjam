a8e558d5aaf870a0db17f179f2c666fc9ed354e0
Julien <julien.lefrancois20@gmail.com>
2025-11-25 15:02:58 +0100

    correctif index et commande du fo

--- html/html/fo/commandes.php
+++ html/html/fo/commandes.php
@@ -7,25 +7,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../css/output.css" >
-    <link rel="stylesheet" type="text/css" href="../../css/fo/general_front.css" >
     <title>Commandes</title>
 </head>
+<?php include __DIR__ . '/../../php/structure/head_front.php'?>
 <body>
     <!--header-->
-    <?php (include __DIR__ . "/../../php/structure/header_front.php"); ?>
-    <?php include(__DIR__ . "/../../php/structure/navbar_front.php"); ?>
+    <?php include __DIR__ . "/../../php/structure/header_front.php"; ?>
+    <?php include __DIR__ . "/../../php/structure/navbar_front.php"; ?>
 
     <main class="min-h-[600px]">
         <h2>Bientôt disponible ...</h2>
         <div class="flex justify-center mt-15 mb-18">
             <a href="../../index.php"><button class="bg-beige shadow rounded-sm md:rounded-2xl w-35 h-10 md:w-50 md:h-14 px-7 cursor-pointer">Retour</button></a>
         </div>
-        <!--
-        <button class="-indent-96 whitespace-nowrap
-                    size-12 md:size-auto md:bg-none md:indent-0 overflow-visible md:whitespace-normal
-                    bg-beige shadow rounded-2xl w-20 h-7 md:w-40 md:h-14 mt-4">
-        <a href="../fo/index.php">Retour</a></button>-->
     </main>
 
     <!--footer-->
