b4e96dd233c0a81df0f5c82a98ac9cd9cc2b126a
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2026-01-05 15:39:01 +0100

    cgu & cgv màj + ml ajouté

--- html/html/bo/cgu_bo.php
+++ html/html/bo/cgu_bo.php
@@ -7,7 +7,7 @@
 <html lang="fr">
 <?php require __DIR__ . "/../../php/structure/head_back.php"; ?>
 <head>
-    <title>Conditions Générales d'Utilisateurs</title>
+    <title>Conditions Générales d'Utilisation</title>
 </head>
 <body>
     <?php
@@ -18,90 +18,46 @@
         <h2>Conditions générales d'utilisation</h2>
         <h3 class="self-center mt-0 mb-2">En vigueur au 07/11/2025</h3>
         <article>
-            <p>Les présentes conditions générales d'utilisation (dites « <strong>CGU</strong> ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par COBREC et de définir les conditions d'accès et d'utilisation des services par « l'<strong>Utilisateur</strong> ».</p>
+            <p>Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par SKADJAM et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ». Les présentes CGU sont accessibles sur le site à la rubrique « CGU ».</p>
             <br>
-            <p>Les présentes CGU sont accessibles sur le site à la rubrique « <strong>CGU</strong> ».</p>
+            <p>Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’Utilisateur. Lors de l'inscription sur le site via le formulaire d’inscription, chaque Utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu et compris les CGU et je les accepte ». En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.</p>
             <br>
-            <p>Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l'utilisateur. Lors de l'inscription sur le site via le Formulaire d'inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu et compris les CGU et je les accepte ». En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site. Alizon.bzh se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.</p>
+            <p>Alizon.bzh se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.</p>
         </article>
         <article>
-            <h3 class="ml-5">Article 1 : Les mentions légales</h3>
-            <p>L'édition du site Alizon.bzh est assurée par la Société SARL COBREC au capital de 0 euros, immatriculée au RCS de Lannion sous le numéro 508 977 303, dont le siège social est situé au 7 Rue Édouard Branly, 22300 Lannion.</p>
-            <p>Numéro de téléphone : 02 96 46 93 00</p>
-            <p>Adresse e-mail : cobrec@mail.bzh</p>
-            <p>Le Directeur de la publication est : Armel KERMANAC'H</p>
+            <h3 class="ml-5">Article 1 : Accès au site</h3>
+            <p>Le site Alizon.bzh permet à l'Utilisateur un accès gratuit aux services suivants :</p>
+            <ul>
+                <li>Créer un compte, se connecter à un compte, se déconnecter d’un compte, supprimer ses données, anonymiser le compte quand il est supprimé, modifier les informations d’un compte.</li>
+                <li>Ajouter des articles dans son panier, supprimer des articles de son panier, vider son panier.</li>
+                <li>Consulter les informations d’un produit, ajouter des articles dans sa liste de souhait, supprimer des articles de sa liste de souhait, filtrer les produits suivant le nom,voir les promotions, voir les produits les plus vendus, voir les nouveaux produits, voir ses commandes en cours et passées.</li>
+                <li>Créer un produit, modifier un produit, supprimer un produit, mettre en ligne un produit, vider son catalogue vendeur, vider le stock d'un produit, modifier le stock d'un produit, consulter le catalogue, promouvoir un produit.</li>
+                <li>Écrire un commentaire et noter un article, vider sa liste de souhait, noter les commentaires d’autres Utilisateurs.</li>
+            </ul>
+            <p>Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion internet, etc.) sont à sa charge.</p>
             <br>
-            <p>L'hébergeur du site Alizon.bzh est la société COBREC, dont le siège social est situé au 7 Rue Édouard Branly, 22300 Lannion, avec le numéro de téléphone : 02 96 46 93 00.</p>
+            <p>Le visiteur n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, le visiteur s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.</p>
+            <br>
+            <p>Pour accéder aux services, le visiteur doit ensuite s'identifier à l'aide de son email et de son mot de passe.</p>
+            <br>
+            <p>Tout Utilisateur inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable. Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de Alizon.bzh. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.</p>
+            <br>
+            <p>L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué dans les mentions légales.</p>
         </article>
         <article>
-            <h3 class="ml-5">Article 2 : Accès au site</h3>
-            <p>Le site Alizon.bzh permet à l'Utilisateur un accès gratuit aux services suivants : E-Commerce</p>
-            <p>Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</p>
-            <br>
-            <p>L'Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s'inscrire en remplissant le formulaire. En acceptant de s'inscrire aux services réservés, l'Utilisateur membre s'engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.</p>
-            <p>Pour accéder aux services, l'Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.</p>
-            <p>Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable. Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de Alizon.bzh. Dans ces cas, l'Utilisateur accepte ainsi ne pas tenir rigueur à l'éditeur de toute interruption ou suspension de service, même sans préavis.</p>
-            <p>L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l'adresse email de l'éditeur communiqué à l'<a href="#mentionsLegalesBO">Article 1</a>.</p>
-        </article>
+            <h3 class="ml-5">Article 2 : Liens hypertextes</h3>
+            <p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site Alizon.bzh. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
         <article>
-            <h3 class="ml-5">Article 3 : Collecte des données</h3>
-            <p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.</p>
-        </article>
-        <article>
-            <h3 class="ml-5">Article 4 : Propriété intellectuelle</h3>
-            <p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
+            <h3 class="ml-5">Article 3 : Publication par l’Utilisateur</h3>
+            <p>Le site permet aux membres de publier les contenus suivants : un commentaire, une photo, une note, un produit. Dans ses publications, le membre s’engage à respecter les règles de la netiquette (règles de bonne conduite de l’internet) et les règles de droit en vigueur.</p>
             <br>
-            <p>L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.</p>
+            <p>Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s’en justifier auprès du membre.</p>
             <br>
-            <p>Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l'autorisation expresse de l'exploitant du site Internet constituerait une contrefaçon sanctionnée par l'article L 335-2 et suivants du Code de la propriété intellectuelle.</p>
+            <p>Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle. Mais en publiant une publication sur le site, il cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé, dans le monde entier, sur tout support (numérique ou physique), pour la durée de la propriété intellectuelle. Le membre cède notamment le droit d'utiliser sa publication sur internet et sur les réseaux de téléphonie mobile. La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa publication.</p>
             <br>
-            <p>Il est rappelé conformément à l'article L122-5 du Code de propriété intellectuelle que l'Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l'auteur et sa source.</p>
-        </article>
-        <article>
-            <h3 class="ml-5">Article 5 : Responsabilité</h3>
-            <p>Les sources des informations diffusées sur le site Alizon.bzh sont réputées fiables mais le site ne garantit pas qu'il soit exempt de défauts, d'erreurs ou d'omissions.</p>
+            <p>Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité.</p>
             <br>
-            <p>Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site Alizon.bzh ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l'utilisation et de l'interprétation de l'information contenue dans ce site. L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.</p>
-            <p>Le site Alizon.bzh ne peut être tenu pour responsable d'éventuels virus qui pourraient infecter l'ordinateur ou tout matériel informatique de l'Internaute, suite à une utilisation, à l'accès, ou au téléchargement provenant de ce site.</p>
-            <br>
-            <p>La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
-        </article>
-        <article>
-            <h3 class="ml-5">Article 6 : Liens hypertextes</h3>
-            <p>Des liens hypertextes peuvent être présents sur le site. L'Utilisateur est informé qu'en cliquant sur ces liens, il sortira du site Alizon.bzh. Ce dernier n'a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
-        </article>
-        <article>
-            <h3 class="ml-5">Article 7 : Cookies</h3>
-            <p>L'Utilisateur est informé que lors de ses visites sur le site, un cookie peut s'installer automatiquement sur son logiciel de navigation.</p>
-            <br>
-            <p>Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l'ordinateur de l'Utilisateur par votre navigateur et qui sont nécessaires à l'utilisation du site Alizon.bzh. Les cookies ne contiennent pas d'information personnelle et ne peuvent pas être utilisés pour identifier quelqu'un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l'Utilisateur, d'autres restent.</p>
-            <br>
-            <p>L'information contenue dans les cookies est utilisée pour améliorer le site Alizon.bzh.</p>
-            <br>
-            <p>En naviguant sur le site, L'Utilisateur les accepte.</p>
-            <p>L'Utilisateur pourra désactiver ces cookies par l'intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
-        </article>
-        <article>
-            <h3 class="ml-5">Article 8 : Publication par l'Utilisateur</h3>
-            <p>Le site permet aux membres de publier les contenus suivants :</p>
-            <p>Produits, Promotions, Commentaires (réponse à un avis).</p>
-            <br>
-            <p>Dans ses publications, le membre s'engage à respecter les règles de la Netiquette (règles de bonne conduite de l'internet) et les règles de droit en vigueur.</p>
-            <br>
-            <p>Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s'en justifier auprès du membre.</p>
-            <br>
-            <p>Le membre reste titulaire de l'intégralité de ses droits de propriété intellectuelle. Mais en publiant une publication sur le site, il cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé, dans le monde entier, sur tout support (numérique ou physique), pour la durée de la propriété intellectuelle. Le Membre cède notamment le droit d'utiliser sa publication sur internet et sur les réseaux de téléphonie mobile.</p>
-            <br>
-            <p>La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa publication.</p>
-            <br>
-            <p>Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.</p>
-            <br>
-            <p>Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site, sans préavis.</p>
-        </article>
-        <article>
-            <h3 class="ml-5">Article 9 : Droit applicable et juridiction compétente</h3>
-            <p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.</p>
-            <p>Pour toute question relative à l'application des présentes CGU, vous pouvez joindre l'éditeur aux coordonnées inscrites à l'<a href="#mentionsLegalesBO">Article 1</a>.</p>
+            <p>L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l’Utilisateur.  Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimée ou modifiée par le site, sans préavis.</p>
         </article>
     </main>
     <?php require __DIR__ . "/../../php/structure/footer_back.php"; ?>
