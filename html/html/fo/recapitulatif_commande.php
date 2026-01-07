7e5894e89571a68c37b453a5d83f1cc56e6e8004
Anarkyia <s.salvadorhoarau@gmail.com>
2026-01-07 17:46:09 +0100

    modif recup infoPanier recapitulatif_commande.php

--- html/html/fo/recapitulatif_commande.php
+++ html/html/fo/recapitulatif_commande.php
@@ -5,16 +5,43 @@
     $idCompte = $_SESSION['idCompte'];
     $idPanier = $_POST['idPanier'];
 
-    $sql = "SELECT *
-    FROM sae3_skadjam._panier
-    WHERE id_panier = :id_panier";
+    $sql = "SELECT 
+            pr.libelle_produit,
+            pr.prix_ttc,
+            v.raison_sociale,
+            d.quantite,
+            d.montant_ht,
+            d.sous_total,
+            p.montant_total_ttc,
+            p.nb_produit_total,
+            SUM(d.sous_total) AS montant_total_ht
+        FROM sae3_skadjam._panier p
+        INNER JOIN sae3_skadjam._contient c
+            ON c.id_panier = p.id_panier
+        INNER JOIN sae3_skadjam._produit pr
+            ON pr.id_produit = c.id_produit
+        INNER JOIN sae3_skadjam._details d
+            ON d.id_produit = p.id_produit       
+        INNER JOIN sae3_skadjam._vendeur v
+            ON v.id_compte = pr.id_vendeur
+        WHERE p.id_panier = :id_panier
+        GROUP BY 
+            pr.libelle_produit,
+            pr.prix_ttc,
+            v.raison_sociale,
+            d.quantite,
+            d.montant_ht,
+            d.sous_total,
+            p.montant_total_ttc,
+            p.nb_produit_total
+        ";
 
     $stmt = $dbh->prepare($sql);
     $stmt->execute([
         ':id_panier' => $idPanier
     ]);
 
-    $panier = $stmt->fetch(PDO::FETCH_ASSOC);
+    $infosPanier = $stmt->fetch(PDO::FETCH_ASSOC);
 
     
     try {
@@ -33,7 +60,7 @@
         $stmtCommande->execute([
             ':etat' => 'Attente de validation',
             ':date_commande' => $date,
-            ':montant_total_ttc' => $panier['montant_total_ttc'],
+            ':montant_total_ttc' => $infosPanier['montant_total_ttc'],
             ':id_client' => $idCompte
         ]);
 
@@ -50,8 +77,8 @@
         $stmtFacture = $dbh->prepare($sqlFacture);
         $stmtFacture->execute([
             ':montant_ht' => 100.42,
-            ':destinataire' => 3,
-            ':date_commande' => '2026-01-07',
+            ':destinataire' => $idCompte,
+            ':date_commande' => $date,
             ':id_commande' => $idCommande
         ]);
 
