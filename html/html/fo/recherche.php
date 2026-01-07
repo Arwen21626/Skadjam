bd2deb935ce5b7b4a2b1a6580f7648b1adbe88bb
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-07 10:08:40 +0100

    ajout des summary

--- html/html/fo/recherche.php
+++ html/html/fo/recherche.php
@@ -26,134 +26,140 @@
                 <h3>Filtres</h3>
                 <!-- Categorie -->
                  <article>
-                    <h4>Par catégorie</h4>
-                    <!-- Alimentaire -->
-                    <div>
-                        <input type="checkbox" name="alimentaire" id="alimentaire">
-                        <label for="alimentaire">Alimentaire</label>
-                    </div>
-                    
+                    <details>
+                        <summary>Par catégorie</summary>
+                        <!-- Alimentaire -->
+                        <div>
+                            <input type="checkbox" name="alimentaire" id="alimentaire">
+                            <label for="alimentaire">Alimentaire</label>
+                        </div>
+                        
 
-                    <!-- Vetements -->
-                    <div>
-                        <input type="checkbox" name="alimentaire" id="alimentaire">
-                        <label for="alimentaire">Vetements</label>
-                    </div>
+                        <!-- Vetements -->
+                        <div>
+                            <input type="checkbox" name="alimentaire" id="alimentaire">
+                            <label for="alimentaire">Vetements</label>
+                        </div>
 
-                    <!-- Artisanat -->
-                    <div>
-                        <input type="checkbox" name="alimentaire" id="alimentaire">
-                        <label for="alimentaire">Artisanat</label>
-                    </div>
+                        <!-- Artisanat -->
+                        <div>
+                            <input type="checkbox" name="alimentaire" id="alimentaire">
+                            <label for="alimentaire">Artisanat</label>
+                        </div>
 
-                    <!-- Goodies -->
-                    <div>
-                        <input type="checkbox" name="alimentaire" id="alimentaire">
-                        <label for="alimentaire">Goodies</label>
-                    </div>
+                        <!-- Goodies -->
+                        <div>
+                            <input type="checkbox" name="alimentaire" id="alimentaire">
+                            <label for="alimentaire">Goodies</label>
+                        </div>
 
-                    <!-- Soin -->
-                    <div>
-                        <input type="checkbox" name="alimentaire" id="alimentaire">
-                        <label for="alimentaire">Soin</label>
-                    </div>
+                        <!-- Soin -->
+                        <div>
+                            <input type="checkbox" name="alimentaire" id="alimentaire">
+                            <label for="alimentaire">Soin</label>
+                        </div>
+                    </details>
                  </article>
                 
-
                 <!-- Notes -->
                 <article>
-                    <h4>Par note</h4>
+                    <details>
+                        <summary>Par note</summary>
+                        <!-- 1 étoile -->
+                        <div>
+                            <input type="checkbox" name="unE" id="unE">
+                            <label for="alimentaire">1</label>
+                        </div>
 
-                    <!-- 1 étoile -->
-                    <div>
-                        <input type="checkbox" name="unE" id="unE">
-                        <label for="alimentaire">1</label>
-                    </div>
-                    
+                        <!-- 2 étoiles -->
+                        <div>
+                            <input type="checkbox" name="deuxE" id="deuxE">
+                            <label for="alimentaire">2</label>
+                        </div>
 
-                    <!-- 2 étoiles -->
-                    <div>
-                        <input type="checkbox" name="deuxE" id="deuxE">
-                        <label for="alimentaire">2</label>
-                    </div>
+                        <!-- 3 étoiles -->
+                        <div>
+                            <input type="checkbox" name="troisE" id="troisE">
+                            <label for="alimentaire">3</label>
+                        </div>
 
-                    <!-- 3 étoiles -->
-                    <div>
-                        <input type="checkbox" name="troisE" id="troisE">
-                        <label for="alimentaire">3</label>
-                    </div>
+                        <!-- 4 étoiles -->
+                        <div>
+                            <input type="checkbox" name="quatreE" id="quatreE">
+                            <label for="alimentaire">4</label>
+                        </div>
 
-                    <!-- 4 étoiles -->
-                    <div>
-                        <input type="checkbox" name="quatreE" id="quatreE">
-                        <label for="alimentaire">4</label>
-                    </div>
-
-                    <!-- 5 étoiles -->
-                    <div>
-                        <input type="checkbox" name="cinqE" id="cinqE">
-                        <label for="alimentaire">5</label>
-                    </div>
+                        <!-- 5 étoiles -->
+                        <div>
+                            <input type="checkbox" name="cinqE" id="cinqE">
+                            <label for="alimentaire">5</label>
+                        </div>
+                    </details>
                 </article>
                 
                 <!-- Tranche de prix -->
                 <article>
-                    <h4>Par tranche de prix</h4>
-
+                    <details>
+                        <summary>Par tranche de prix</summary>
+                    </details>
                 </article>
-
             </section>
 
             <section>
                 <h3>Tris</h3>
                 <!-- prix -->
                 <article>
-                    <h4>Par prix</h4>
-                    <div>
+                    <details>
+                        <summary>Par prix</summary>
                         <div>
-                            <input type="radio" name="prixTri" id="prixTri">
-                            <label for="prixTri">Croissant</label>
+                            <div>
+                                <input type="radio" name="prixTri" id="prixTri">
+                                <label for="prixTri">Croissant</label>
+                            </div>
+                            <div>
+                                <input type="radio" name="prixTri" id="prixTri">
+                                <label for="prixTri">Décroissant</label>
+                            </div>
                         </div>
-                        <div>
-                            <input type="radio" name="prixTri" id="prixTri">
-                            <label for="prixTri">Décroissant</label>
-                        </div>
-                    </div>
+                    </details>
+                    
                 </article>
 
                 <!-- ordre alpha -->
                 <article>
-                    <h4>Par ordre alphabétique</h4>
-                    <div>
+                    <details>
+                        <summary>Par ordre alphabétique</summary>
                         <div>
-                            <input type="radio" name="alphaTri" id="alphaTri">
-                            <label for="alphaTri">A-Z</label>
+                            <div>
+                                <input type="radio" name="alphaTri" id="alphaTri">
+                                <label for="alphaTri">A-Z</label>
+                            </div>
+                            <div>
+                                <input type="radio" name="alphaTri" id="alphaTri">
+                                <label for="alphaTri">Z-A</label>
+                            </div>
                         </div>
-                        <div>
-                            <input type="radio" name="alphaTri" id="alphaTri">
-                            <label for="alphaTri">Z-A</label>
-                        </div>
-                    </div>
+                    </details>
+                    
                 </article>
+
                 <!-- note -->
                 <article>
-                    <h4>Par note</h4>
-                    <div>
+                    <details>
+                        <summary>Par note</summary>
                         <div>
-                            <input type="radio" name="noteTri" id="noteTri">
-                            <label for="noteTri">5-1</label>
+                            <div>
+                                <input type="radio" name="noteTri" id="noteTri">
+                                <label for="noteTri">5-1</label>
+                            </div>
+                            <div>
+                                <input type="radio" name="noteTri" id="noteTri">
+                                <label for="noteTri">1-5</label>
+                            </div>
                         </div>
-                        <div>
-                            <input type="radio" name="noteTri" id="noteTri">
-                            <label for="noteTri">1-5</label>
-                        </div>
-                    </div>
+                    </details>
                 </article>
-                
             </section>
-            
-
-            
         </aside>
 
 
