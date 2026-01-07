8c9d06dc35ae13a090d6b33bea542202caf497f5
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-07 09:57:59 +0100

    Barre de recherche et ajout script js associé

--- html/html/fo/recherche.php
+++ html/html/fo/recherche.php
@@ -18,19 +18,150 @@
     <?php (include __DIR__ . "/../../php/structure/header_front.php"); ?>
     <?php include(__DIR__ . "/../../php/structure/navbar_front.php"); ?>
 
-    <main class="min-h-[600px]">
-        <h2>Bientôt disponible ...</h2>
-        <div class="flex justify-center mt-15 mb-18">
-            <button class="bg-beige shadow rounded-sm md:rounded-2xl w-35 h-10 md:w-50 md:h-14 px-7"><a href="../../index.php">Retour</a></button>
-        </div>
-        <!--
-        <button class="-indent-96 whitespace-nowrap
-                    size-12 md:size-auto md:bg-none md:indent-0 overflow-visible md:whitespace-normal
-                    bg-beige shadow rounded-2xl w-20 h-7 md:w-40 md:h-14 mt-4">
-        <a href="../fo/index.php">Retour</a></button>-->
+    <main class="md:min-h-[800px] min-h-[600px]">
+        <!-- Barre de recherche -->
+        <aside class="sidebar">
+            <!-- Filtres -->
+            <section>
+                <h3>Filtres</h3>
+                <!-- Categorie -->
+                 <article>
+                    <h4>Par catégorie</h4>
+                    <!-- Alimentaire -->
+                    <div>
+                        <input type="checkbox" name="alimentaire" id="alimentaire">
+                        <label for="alimentaire">Alimentaire</label>
+                    </div>
+                    
+
+                    <!-- Vetements -->
+                    <div>
+                        <input type="checkbox" name="alimentaire" id="alimentaire">
+                        <label for="alimentaire">Vetements</label>
+                    </div>
+
+                    <!-- Artisanat -->
+                    <div>
+                        <input type="checkbox" name="alimentaire" id="alimentaire">
+                        <label for="alimentaire">Artisanat</label>
+                    </div>
+
+                    <!-- Goodies -->
+                    <div>
+                        <input type="checkbox" name="alimentaire" id="alimentaire">
+                        <label for="alimentaire">Goodies</label>
+                    </div>
+
+                    <!-- Soin -->
+                    <div>
+                        <input type="checkbox" name="alimentaire" id="alimentaire">
+                        <label for="alimentaire">Soin</label>
+                    </div>
+                 </article>
+                
+
+                <!-- Notes -->
+                <article>
+                    <h4>Par note</h4>
+
+                    <!-- 1 étoile -->
+                    <div>
+                        <input type="checkbox" name="unE" id="unE">
+                        <label for="alimentaire">1</label>
+                    </div>
+                    
+
+                    <!-- 2 étoiles -->
+                    <div>
+                        <input type="checkbox" name="deuxE" id="deuxE">
+                        <label for="alimentaire">2</label>
+                    </div>
+
+                    <!-- 3 étoiles -->
+                    <div>
+                        <input type="checkbox" name="troisE" id="troisE">
+                        <label for="alimentaire">3</label>
+                    </div>
+
+                    <!-- 4 étoiles -->
+                    <div>
+                        <input type="checkbox" name="quatreE" id="quatreE">
+                        <label for="alimentaire">4</label>
+                    </div>
+
+                    <!-- 5 étoiles -->
+                    <div>
+                        <input type="checkbox" name="cinqE" id="cinqE">
+                        <label for="alimentaire">5</label>
+                    </div>
+                </article>
+                
+                <!-- Tranche de prix -->
+                <article>
+                    <h4>Par tranche de prix</h4>
+
+                </article>
+
+            </section>
+
+            <section>
+                <h3>Tris</h3>
+                <!-- prix -->
+                <article>
+                    <h4>Par prix</h4>
+                    <div>
+                        <div>
+                            <input type="radio" name="prixTri" id="prixTri">
+                            <label for="prixTri">Croissant</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="prixTri" id="prixTri">
+                            <label for="prixTri">Décroissant</label>
+                        </div>
+                    </div>
+                </article>
+
+                <!-- ordre alpha -->
+                <article>
+                    <h4>Par ordre alphabétique</h4>
+                    <div>
+                        <div>
+                            <input type="radio" name="alphaTri" id="alphaTri">
+                            <label for="alphaTri">A-Z</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="alphaTri" id="alphaTri">
+                            <label for="alphaTri">Z-A</label>
+                        </div>
+                    </div>
+                </article>
+                <!-- note -->
+                <article>
+                    <h4>Par note</h4>
+                    <div>
+                        <div>
+                            <input type="radio" name="noteTri" id="noteTri">
+                            <label for="noteTri">1-5</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="noteTri" id="noteTri">
+                            <label for="noteTri">5-1</label>
+                        </div>
+                    </div>
+                </article>
+                
+            </section>
+            
+
+            
+        </aside>
+
+
+        
     </main>
 
     <!--footer-->
     <?php include (__DIR__ . "/../../php/structure/footer_front.php"); ?>
+    <script></script>
 </body>
 </html>
