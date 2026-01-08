103667e512c2697b07ba146b701325789ae17b99
Julien <julien.lefrancois20@gmail.com>
2026-01-08 08:45:43 +0100

    ajout css page recherche

--- html/html/fo/recherche.php
+++ html/html/fo/recherche.php
@@ -52,7 +52,7 @@
     <?php (include __DIR__ . "/../../php/structure/header_front.php"); ?>
     <?php include(__DIR__ . "/../../php/structure/navbar_front.php"); ?>
 
-    <main class="md:min-h-[800px] min-h-[600px]">
+    <main class="md:min-h-[800px] min-h-[600px] mt-10">
         <!-- Barre de recherche -->
         <aside class="sidebar w-60 p-5 bg-beige">
             <!-- Filtres -->
@@ -71,26 +71,26 @@
 
                         <!-- Vetements -->
                         <div>
-                            <input type="checkbox" name="alimentaire" id="alimentaire">
-                            <label for="alimentaire">Vetements</label>
+                            <input type="checkbox" name="Vetement" id="Vetement">
+                            <label for="Vetement">Vetements</label>
                         </div>
 
                         <!-- Artisanat -->
                         <div>
-                            <input type="checkbox" name="alimentaire" id="alimentaire">
-                            <label for="alimentaire">Artisanat</label>
+                            <input type="checkbox" name="Artisanat" id="Artisanat">
+                            <label for="Artisanat">Artisanat</label>
                         </div>
 
                         <!-- Goodies -->
                         <div>
-                            <input type="checkbox" name="alimentaire" id="alimentaire">
-                            <label for="alimentaire">Goodies</label>
+                            <input type="checkbox" name="Goodies" id="Goodies">
+                            <label for="Goodies">Goodies</label>
                         </div>
 
                         <!-- Soin -->
                         <div>
-                            <input type="checkbox" name="alimentaire" id="alimentaire">
-                            <label for="alimentaire">Soin</label>
+                            <input type="checkbox" name="Soin" id="Soin">
+                            <label for="Soin">Soin</label>
                         </div>
                     </details>
                  </article>
@@ -102,31 +102,31 @@
                         <!-- 1 étoile -->
                         <div>
                             <input type="checkbox" name="unE" id="unE">
-                            <label for="alimentaire">1</label>
+                            <label for="unE">1</label>
                         </div>
 
                         <!-- 2 étoiles -->
                         <div>
                             <input type="checkbox" name="deuxE" id="deuxE">
-                            <label for="alimentaire">2</label>
+                            <label for="deuxE">2</label>
                         </div>
 
                         <!-- 3 étoiles -->
                         <div>
                             <input type="checkbox" name="troisE" id="troisE">
-                            <label for="alimentaire">3</label>
+                            <label for="troisE">3</label>
                         </div>
 
                         <!-- 4 étoiles -->
                         <div>
                             <input type="checkbox" name="quatreE" id="quatreE">
-                            <label for="alimentaire">4</label>
+                            <label for="quatreE">4</label>
                         </div>
 
                         <!-- 5 étoiles -->
                         <div>
                             <input type="checkbox" name="cinqE" id="cinqE">
-                            <label for="alimentaire">5</label>
+                            <label for="cinqE">5</label>
                         </div>
                     </details>
                 </article>
@@ -135,6 +135,26 @@
                 <article>
                     <details>
                         <summary class="cursor-pointer mt-1 mb-1">Par tranche de prix</summary>
+                        <div>
+                            <input type="checkbox" name="prix1" id="prix1">
+                            <label for="prix1">2,99€ - 8,39€ </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="prix2" id="prix2">
+                            <label for="prix2">8,40€ - 13,19€ </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="prix3" id="prix3">
+                            <label for="prix3">13,20€ - 19,19€ </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="prix4" id="prix4">
+                            <label for="prix4">19,20€ - 31,19€ </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="prix5" id="prix5">
+                            <label for="prix5">31,19€ - 71,99€ </label>
+                        </div>
                     </details>
                 </article>
             </section>
@@ -147,12 +167,12 @@
                         <summary class="cursor-pointer mt-1 mb-1">Par prix</summary>
                         <div>
                             <div>
-                                <input type="radio" name="prixTri" id="prixTri">
-                                <label for="prixTri">Croissant</label>
+                                <input type="radio" name="prixTri" id="prixTriCroissant">
+                                <label for="prixTriCroissant">Croissant</label>
                             </div>
                             <div>
-                                <input type="radio" name="prixTri" id="prixTri">
-                                <label for="prixTri">Décroissant</label>
+                                <input type="radio" name="prixTri" id="prixTriDecroissant">
+                                <label for="prixTriDecroissant">Décroissant</label>
                             </div>
                         </div>
                     </details>
@@ -165,12 +185,12 @@
                         <summary class="cursor-pointer mt-1 mb-1">Par ordre alphabétique</summary>
                         <div>
                             <div>
-                                <input type="radio" name="alphaTri" id="alphaTri">
-                                <label for="alphaTri">A-Z</label>
+                                <input type="radio" name="alphaTri" id="alphaTriAZ">
+                                <label for="alphaTriAZ">A-Z</label>
                             </div>
                             <div>
-                                <input type="radio" name="alphaTri" id="alphaTri">
-                                <label for="alphaTri">Z-A</label>
+                                <input type="radio" name="alphaTri" id="alphaTriZA">
+                                <label for="alphaTriZA">Z-A</label>
                             </div>
                         </div>
                     </details>
@@ -183,12 +203,12 @@
                         <summary class="cursor-pointer mt-1 mb-1">Par note</summary>
                         <div>
                             <div>
-                                <input type="radio" name="noteTri" id="noteTri">
-                                <label for="noteTri">5-1</label>
+                                <input type="radio" name="noteTri" id="noteTri51">
+                                <label for="noteTri51">5-1</label>
                             </div>
                             <div>
-                                <input type="radio" name="noteTri" id="noteTri">
-                                <label for="noteTri">1-5</label>
+                                <input type="radio" name="noteTri" id="noteTri15">
+                                <label for="noteTri15">1-5</label>
                             </div>
                         </div>
                     </details>
@@ -196,62 +216,66 @@
             </section>
         </aside>
 
-        <script>
-            // Boucle pour afficher tous les produits
-            tabProd.forEach(prod => {   
-                idProduit = prod['id_produit']
-                let parent = document.getElementsByTagName("main")[0]
+        <div class="grid grid-cols-2 justify-items-center md:grid-cols-3">
+            <script>
+                // Boucle pour afficher tous les produits
+                tabProd.forEach(prod => {
+                    idProduit = prod['id_produit']
+                    let parent = document.getElementsByTagName("main")[0]
 
-                // Pour avoir seulement le main et pas le tableau renvoyé
+                    // Pour avoir seulement le main et pas le tableau renvoyé
 
-                // Section   
-                let produit = document.createElement("section")
-                parent.appendChild(produit)
+                    // Section   
+                    let produit = document.createElement("section")
+                    parent.appendChild(produit)
+                    produit.classList.add("bg-bleu", "grid", "grid-cols-[40%_60%]", "w-40", "md:w-80", "h-auto", "p-2", "md:p-3", "m-2")
+                    parent = produit
 
-                parent = produit
-                
+                    //Lien
+                    let lien = document.createElement("a")
+                    lien.href = "details_produit.php?idProduit="+idProduit
+                    lien.classList.add("col-span-2", "justify-self-center", "mb-3");
+                    parent.appendChild(lien)
 
-                //Lien
-                let lien = document.createElement("a")
-                lien.href = "details_produit.php?idProduit="+idProduit
-                parent.appendChild(lien)
+                    parent = lien
 
-                parent = lien
+                    // Image
+                    let image = document.createElement("img")
+                    image.src = prod['url_photo']
+                    image.alt = prod['alt']
+                    image.title = prod['title']
+                    parent.appendChild(image)
 
-                // Image
-                let image = document.createElement("img")
-                image.src = prod['url_photo']
-                image.alt = prod['alt']
-                image.title = prod['title']
-                parent.appendChild(image)
+                    // Nom produit
+                    let nom = document.createElement("p")
+                    nom.textContent = prod['libelle_produit']
+                    parent.appendChild(nom)
+                    nom.classList.add("col-span-2")
 
-                // Nom produit
-                let nom = document.createElement("p")
-                nom.textContent = prod['libelle_produit']
-                parent.appendChild(nom)
+                    // Prix et note
+                    let contient = document.createElement("div")
+                    parent.appendChild(contient)
+                    contient.classList.add("flex", "justify-start", "items-center", "col-span-2")
 
-                // Prix et note
-                let contient = document.createElement("div")
-                parent.appendChild(contient)
+                    parent = contient
 
-                parent = contient
+                    // Prix
+                    let prix = document.createElement("p")
+                    prix.textContent = prod['prix_ttc']+" €"
+                    parent.appendChild(prix)
 
-                // Prix
-                let prix = document.createElement("p")
-                prix.textContent = prod['prix_ttc']+" €"
-                parent.appendChild(prix)
+                    // Note
+                    let contientNote = document.createElement("div")
+                    parent.appendChild(contientNote)
+                    contientNote.classList.add("w-2/4", "ml-2", "md:ml-10", "flex")
 
-                // Note
-                let contientNote = document.createElement("div")
-                parent.appendChild(contientNote)
-
-                parent = contientNote
-                // console.log(parent)
-            });
+                    parent = contientNote
+                    // console.log(parent)
+                });
 
 
-        </script>
-
+            </script>
+        </div>
         <!-- str_replace-->
         <div class="w-2/4 ml-2 md:ml-10 flex">
             <?php 
