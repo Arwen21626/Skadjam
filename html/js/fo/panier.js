ae9cde74cc8cb1ff8eb237a139d7cfea62856473
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2026-01-11 19:13:56 +0100

    Autre méthode de correction panier

--- html/js/fo/panier.js
+++ html/js/fo/panier.js
@@ -13,27 +13,27 @@ document.querySelectorAll('.produit').forEach(container => {
     let prixTTC = Number(container.querySelector('.prod-info').querySelector('.prix').querySelector('.prix-u').textContent.slice(17, -1).replace(',', '.'));
     let prixTot = container.querySelector('.prod-info').querySelector('.prix').querySelector('.prix-tot'); //Prix total d'un produit
 
-    btnRetrait.addEventListener('mousedown', e => {
-        e.preventDefault();
+    let oldValue = input.value;
+
+    btnRetrait.addEventListener('click', e => {
 
         if (input.value > 1) {
             input.value = Number(input.value) - 1;
             prixTot.textContent = ("Prix total : " + (prixTTC * Number(input.value)).toFixed(2) + "€").replace('.', ',');
             nbProdTot.textContent = Number(nbProdTot.textContent) - 1;
             sousTotal.textContent = ((Number(sousTotal.textContent.slice(0, -1).replace(',','.')) - prixTTC).toFixed(2) + "€").replace('.', ',');
+            oldValue = input.value;
         }
     });
 
-    btnAjout.addEventListener('mousedown', e => {
-        e.preventDefault();
+    btnAjout.addEventListener('click', e => {
 
         input.value = Number(input.value) + 1;
         prixTot.textContent = ("Prix total : " + (prixTTC * Number(input.value)).toFixed(2) + "€").replace('.', ',');
         nbProdTot.textContent = Number(nbProdTot.textContent) + 1;
         sousTotal.textContent = ((Number(sousTotal.textContent.slice(0, -1).replace(',','.')) + prixTTC).toFixed(2) + "€").replace('.', ',');
+        oldValue = input.value;
     });
-
-    let oldValue = input.value;
 
     input.addEventListener('input', () => {
         let filtre = input.value.replace(/[^0-9]*/g, ''); // Empêche la saisie de tout caractères autres que 0-9
