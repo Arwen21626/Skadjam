037e3bef678866882e4bcc52a76fb5fb16cec255
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-12 14:37:37 +0100

    Retrait lien inutile CSS et JS

--- html/html/fo/recherche.php
+++ html/html/fo/recherche.php
@@ -26,11 +26,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../css/output.css" >
-    <link rel="stylesheet" type="text/css" href="../../css/fo/general_front.css" >
     <title>Recherche</title>
     <?php include __DIR__ . "/../../php/structure/head_front.php"; ?>
     <script src="../../js/fo/recherche.js"></script>
-    <script src="../../js/fo/julien.js"></script>
 </head>
 
 <body>
