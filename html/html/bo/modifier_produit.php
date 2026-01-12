e0f73d05fba9decba63f3de5382260dec6b4ec85
Diwezha <diwezha.ruveron@gmail.com>
2026-01-12 17:30:37 +0100

    correction lien

--- html/html/bo/modifier_produit.php
+++ html/html/bo/modifier_produit.php
@@ -4,7 +4,6 @@ include __DIR__ . '/../../php/verif_role_bo.php';
 include  __DIR__ . '/../../01_premiere_connexion.php';
 require_once __DIR__ . '/../../php/verification_formulaire.php';
 require_once __DIR__ . '/../../php/modification_variable.php';
-require_once(__DIR__ . '/../../php/modifier_remise.php');
 
 $idProduit = $_GET['idProduit'];
 $idCompte = $_SESSION['idCompte'];
