6df19bc968cfac70e5f76c6e9869f48c44d3e060
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-14 09:30:16 +0100

    Ajustement couleur texte

--- html/php/structure/navbar_back.php
+++ html/php/structure/navbar_back.php
@@ -37,7 +37,7 @@
                 </div>
             </a>
 
-            <a class="text-vertFonce hover:text-rouge hidden md:inline-block" href="/html/fo/recherche.php">Recherche</a>
+            <a class="text-bleu hover:text-rouge hidden md:inline-block" href="/html/fo/recherche.php">Recherche</a>
         </div>
 
 
