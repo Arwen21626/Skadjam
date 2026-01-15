c2a409fb074f6a5c8f74add6a13e774aeacc8e8e
Diwezha <diwezha.ruveron@gmail.com>
2026-01-15 11:13:58 +0100

    libelle_produit

--- html/html/bo/index_vendeur.php
+++ html/html/bo/index_vendeur.php
@@ -69,7 +69,7 @@
 
             try {                
                 //récupère toutes les infos des tables produits et photos
-                foreach($dbh->query("SELECT pr.id_produit, url_photo, alt, titre, prix_ttc, quantite_stock, note_moyenne, prix_remise, pourcentage_remise
+                foreach($dbh->query("SELECT pr.libelle_produit, pr.id_produit, url_photo, alt, titre, prix_ttc, quantite_stock, note_moyenne, prix_remise, pourcentage_remise
                                     FROM sae3_skadjam._produit pr
                                     INNER join sae3_skadjam._montre m
                                         ON pr.id_produit=m.id_produit
