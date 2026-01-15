493c8e0e6706276c26b2ccffcbd4ed25a4be4912
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-15 16:10:05 +0100

    Ajout de fichiers js pour la pagination et les variables

---
+++ html/js/variables.js
@@ -0,0 +1,26 @@
+// Variables et constantes pour la pagination
+const numberOfItems = 24
+let first = 0
+let actualPage = 1
+
+
+
+let tableau = []
+
+// Variables et constantes pour les filtres
+var checkedCategories = []
+var checkedNotes = []
+var checkedTranches = []
+
+let ali = false
+let vet = false
+let art = false
+let goo = false
+let soi = false
+
+let not0 = false
+let not1 = false
+let not2 = false
+let not3 = false
+let not4 = false
+let not5 = false
