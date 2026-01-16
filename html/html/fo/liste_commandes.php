10b37f164cece09c1a7fd2da66d35e23609b8a08
Anarkyia <s.salvadorhoarau@gmail.com>
2026-01-16 14:05:10 +0100

    a

--- html/html/fo/liste_commandes.php
+++ html/html/fo/liste_commandes.php
@@ -5,7 +5,7 @@
     $idCompte = $_SESSION['idCompte'];
 
     if($idCompte == null){
-        die("Erreur : Vous n'êtes pas connecté.");
+        header('Location: ./connexion.php');
     }
 
     try {     
