7b021bb801d786a3d0dd07d437ec8777f32c4f94
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-16 14:40:14 +0100

    KDO Geuli1 fais tes tests

--- html/js/fo/recherche.js
+++ html/js/fo/recherche.js
@@ -182,72 +182,72 @@ function ajoutEventListener(){
     
 }
 
-function afficherProduit(indice){
+// function afficherProduit(indice){
     
-    let i = indice
+//     let i = indice
     
-    let idProduit = tableau[i]['id_produit']
-    let parent = document.getElementById("prod")
+//     let idProduit = tableau[i]['id_produit']
+//     let parent = document.getElementById("prod")
 
-    // Section   
-    let produit = document.createElement("section")
-    parent.appendChild(produit)
-    produit.classList.add("bg-bleu","flex", "flex-col", "w-40", "h-auto","p-2", "m-2", "md:w-80", "md:p-3")
-    parent = produit
+//     // Section   
+//     let produit = document.createElement("section")
+//     parent.appendChild(produit)
+//     produit.classList.add("bg-bleu","flex", "flex-col", "w-40", "h-auto","p-2", "m-2", "md:w-80", "md:p-3")
+//     parent = produit
 
-    //Lien
-    let lien = document.createElement("a")
-    lien.href = "details_produit.php?idProduit="+idProduit
-    lien.classList.add("mb-3");
-    parent.appendChild(lien)
+//     //Lien
+//     let lien = document.createElement("a")
+//     lien.href = "details_produit.php?idProduit="+idProduit
+//     lien.classList.add("mb-3");
+//     parent.appendChild(lien)
 
-    parent = lien
+//     parent = lien
 
-    // Image
-    let image = document.createElement("img")
-    image.src = tableau[i]['url_photo']
-    image.alt = tableau[i]['alt']
-    image.title = tableau[i]['title']
-    image.classList.add("w-auto", "h-40", "md:h-80", "justify-self-center")
-    parent.appendChild(image)
+//     // Image
+//     let image = document.createElement("img")
+//     image.src = tableau[i]['url_photo']
+//     image.alt = tableau[i]['alt']
+//     image.title = tableau[i]['title']
+//     image.classList.add("w-auto", "h-40", "md:h-80", "justify-self-center")
+//     parent.appendChild(image)
 
-    // Nom produit
-    let nom = document.createElement("p")
-    nom.textContent = tableau[i]['libelle_produit']
-    parent.appendChild(nom)
+//     // Nom produit
+//     let nom = document.createElement("p")
+//     nom.textContent = tableau[i]['libelle_produit']
+//     parent.appendChild(nom)
 
-    // Prix et note
-    let contientPrix = document.createElement("article")
-    parent.appendChild(contientPrix)
-    contientPrix.classList.add("flex","flex-row", "justify-between", "items-center")
+//     // Prix et note
+//     let contientPrix = document.createElement("article")
+//     parent.appendChild(contientPrix)
+//     contientPrix.classList.add("flex","flex-row", "justify-between", "items-center")
 
-    parent = contientPrix
+//     parent = contientPrix
 
-    // Prix TTC
-    let prixTTC = document.createElement("p")
-    prixTTC.textContent = tableau[i]['prix_ttc'].replace(".", ",")+" € (TTC)"
-    prixTTC.classList.add("line-through")
-    parent.appendChild(prixTTC)
+//     // Prix TTC
+//     let prixTTC = document.createElement("p")
+//     prixTTC.textContent = tableau[i]['prix_ttc'].replace(".", ",")+" € (TTC)"
+//     prixTTC.classList.add("line-through")
+//     parent.appendChild(prixTTC)
 
-    let prixRemise = document.createElement("p")
-    prixRemise.textContent = tableau[i]['prix_remise'].replace(".", ",")+" € (TTC)"
-    parent.appendChild(prixRemise)
+//     let prixRemise = document.createElement("p")
+//     prixRemise.textContent = tableau[i]['prix_remise'].replace(".", ",")+" € (TTC)"
+//     parent.appendChild(prixRemise)
 
-    if (tableau[i]['prix_remise'] == tableau[i]['prix_ttc']) {
-        prixRemise.classList.add("hidden")
-        prixTTC.classList.remove("line-through")
-    }
+//     if (tableau[i]['prix_remise'] == tableau[i]['prix_ttc']) {
+//         prixRemise.classList.add("hidden")
+//         prixTTC.classList.remove("line-through")
+//     }
 
-    // Note
-    let contientNote = document.createElement("div")
-    parent = lien   
-    parent.appendChild(contientNote)
-    contientNote.classList.add("flex")
+//     // Note
+//     let contientNote = document.createElement("div")
+//     parent = lien   
+//     parent.appendChild(contientNote)
+//     contientNote.classList.add("flex")
 
-    parent = contientNote
+//     parent = contientNote
 
-    let note = tableau[i]['note_moyenne']
-    affichageNote(note, parent)
+//     let note = tableau[i]['note_moyenne']
+//     affichageNote(note, parent)
 
     //setTimeout(function(){console.log('Code waits for 1  second')}, 1000);
-}
+// }
