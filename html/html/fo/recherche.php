a042784ec4d8f1ed1a48841d7ce50ab4eb35ee47
Julien <julien.lefrancois20@gmail.com>
2026-01-16 14:09:33 +0100

    function filtre

--- html/html/fo/recherche.php
+++ html/html/fo/recherche.php
@@ -140,7 +140,7 @@
                         <summary class="cursor-pointer mt-1 mb-1">Par tranche de prix</summary>
                         <div>
                             <input type="checkbox" name="prix1" id="prix1" value="prix1" class="h-5 w-5">
-                            <label for="prix1" class="labelDetails">2,99€ - 8,39€ </label>
+                            <label for="prix1" class="labelDetails">0€ - 8,39€ </label>
                         </div>
                         <div>
                             <input type="checkbox" name="prix2" id="prix2" value="prix2" class="h-5 w-5">
