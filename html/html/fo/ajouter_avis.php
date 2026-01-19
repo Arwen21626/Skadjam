d2962efa09da9faa355945f929c1eb59e448dcdc
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-01-19 17:32:07 +0100

    same

--- html/html/fo/ajouter_avis.php
+++ html/html/fo/ajouter_avis.php
@@ -26,7 +26,7 @@
 
             if ($nouvNote>=0 && $nouvNote<=5){
                 // si c'est l'ajout d'un nouvel avis
-                if ($note === null){
+                if ($nouvNote === null){
                     $insertionAvis = $dbh->prepare("INSERT INTO sae3_skadjam._avis(nb_etoile, nb_pouce_haut, nb_pouce_bas, contenu_commentaire, id_produit, id_compte) 
                                                     VALUES ($nouvNote, 0, 0, '$nouvCommentaire', $idProd, $idCompte)");
                 }
