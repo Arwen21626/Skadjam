ad4e63aca72fc918833d15047284e89aadc06a97
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2026-01-19 16:26:00 +0100

    ?php en ?=

--- html/index.php
+++ html/index.php
@@ -93,18 +93,18 @@
                         <section class="bg-bleu flex flex-col w-40 h-auto p-2 m-2 md:w-80 md:p-3">
                             <!--affichage de la photo-->
                             <a href= "<?php echo "html/fo/details_produit.php?idProduit=".$idProduit;?>" class="mb-3">
-                                <img src="<?php echo $valeurs['url_photo'];?>" 
-                                        alt="<?php echo $valeurs['alt'];?>"
-                                        title="<?php echo $valeurs['titre'];?>"
-                                        class="w-auto h-40 md:h-80 justify-self-center">
+                                <img src="<?= $valeurs['url_photo'];?>" 
+                                        alt="<?= $valeurs['alt'];?>"
+                                        title="<?= $valeurs['titre'];?>"
+                                        class="w-full h-40 md:h-80 justify-self-center">
 
                                 <!--affichage du nom du produit-->
-                                <p><?php echo $valeurs['libelle_produit'];?></p> 
+                                <p><?= $valeurs['libelle_produit'];?></p> 
 
                                 <!--affichage du prix du produit-->   
                                 <div class="flex flex-row justify-between items-center">
-                                    <p class="inline-block <?php echo ($valeurs['pourcentage_remise'] !== NULL)?'line-through':'';?>"> <?php echo htmlentities(str_replace(".", ",",$valeurs['prix_ttc'])); ?>€ (TTC)</p>
-                                    <p class=" pl-3 <?php echo ($valeurs['pourcentage_remise'] !== NULL)?'':'hidden';?>"> <?php echo htmlentities(str_replace(".", ",",$valeurs['prix_remise'])); ?>€ (TTC)</p>
+                                    <p class="inline-block <?= ($valeurs['pourcentage_remise'] !== NULL)?'line-through':'';?>"> <?= htmlentities(str_replace(".", ",",$valeurs['prix_ttc'])); ?>€ (TTC)</p>
+                                    <p class=" pl-3 <?= ($valeurs['pourcentage_remise'] !== NULL)?'':'hidden';?>"> <?= htmlentities(str_replace(".", ",",$valeurs['prix_remise'])); ?>€ (TTC)</p>
                                 </div>
                                 <!--récupération de la note-->
                                 <div class="flex">
