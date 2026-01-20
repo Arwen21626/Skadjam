441a743e81b28a159006bbe1354311be63e24526
Julien <julien.lefrancois20@gmail.com>
2026-01-20 08:54:41 +0100

    modifiaction fo details produit

--- html/html/fo/details_produit.php
+++ html/html/fo/details_produit.php
@@ -208,7 +208,7 @@
                             $stmt->execute([$idAvis]);
                             $reponse = $stmt->fetch(PDO::FETCH_ASSOC);
                             
-                            $aReponse = ($reponse['id_avis']!==null)?true:false;
+                            $aReponse = (isset($reponse['id_avis']))?true:false;
                             ?>
                             <section class=" bg-bleu m-4 p-4 md:w-4xl <?php echo $aReponse?'mb-0 rounded-t-2xl':'rounded-2xl'?>">
 
@@ -247,7 +247,7 @@
                                 </div>
                                 <p><?php echo $row['contenu_commentaire'];?></p>     
                             </section>
-                            <?php if($reponse["id_avis"] !== null){?>
+                            <?php if(isset($reponse["id_avis"])){?>
                             <section class=" bg-beige m-4 mt-0 p-4 w-4xl rounded-b-2xl">
                                 <div class="grid grid-cols-4 md:grid-cols-5 justify-items-end w-auto">
                                     <h4 class="mr-4 col-span-2 md:col-span-3 justify-self-start">
