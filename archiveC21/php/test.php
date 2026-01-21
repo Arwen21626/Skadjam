aeb1f5f0f93731f8626438df1c69e40471b562f7
Seraphim5012 <mewen.roussel@hotmail.com>
2026-01-21 09:41:33 +0100

    test

--- archiveC21/php/test.php
+++ archiveC21/php/test.php
@@ -3,7 +3,7 @@
 include __DIR__ . '/recupraptor.php';
 
 $rpr = new Recupraptor("127.0.0.1", 6969, "test", "1234");
-$ret = $rpr->create_bord("14222", "alison", "4 avenue fosh", 22300, "roussel", "mewen", "5 rue machin", 22450);
+$ret = $rpr->create_bord("14222", "alison");
 echo $ret;
 $etat = $rpr->get_etat($ret);
 echo $etat;
