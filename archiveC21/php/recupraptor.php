99057470ad18f9dc17c90707e586decc07d802ba
Julien <julien.lefrancois20@gmail.com>
2026-01-21 12:28:36 +0100

    a

--- archiveC21/php/recupraptor.php
+++ archiveC21/php/recupraptor.php
@@ -27,6 +27,7 @@ class Recupraptor{
         print_r($this->ip);
         print_r($this->port);
         $this->conn = fsockopen($this->ip, $this->port, $errno, $errstr);
+        print_r("conn : ");
         print_r($this->conn);
         print_r("fin connexion");
         if (!$this->conn){
