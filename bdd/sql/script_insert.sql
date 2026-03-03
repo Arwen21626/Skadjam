76b986bc8b1cf7297f97fe2e2172cd4ccfee40d7
Korentin Parveau Le Saint <134099909+SunFry@users.noreply.github.com>
2026-03-03 17:19:12 +0100

    re correction (g oublié la virgule zebi)

--- bdd/sql/script_insert.sql
+++ bdd/sql/script_insert.sql
@@ -44,7 +44,7 @@ VALUES
   ('Bouvet','Bertrand','pmallet@bouygtel.fr','$2y$10$17I6E4cbfquAL0PElM98a.Dez7My6EdvNnKbQvwu.fkOnM4wL5rEm','+33430320910',false),
   ('Carpentier','Clémence','lalbert@live.com','$2y$10$XkHC12lYBQ6X58G6T9n/i.TDg1Mh47.zzURqYQstZaiElU.7fYYUi','+33428865033',false),
   ('Couturier','Laure','rolandgrenier@blanchet.com','$2y$10$SxzBbx9XdTKlcWv1wZbn3ul8K49YnnXlMCMwIEYYHqybfhAQR1v/2','+33551471854',false),
-  ('Anonyme','Anonyme','anonyme@anonyme.ano','0','+33101010101',false)
+  ('Anonyme','Anonyme','anonyme@anonyme.ano','0','+33101010101',false),
   ('Anonyme','Anonyme','anonyme_vendeur@anonyme.ano','0','+33101010101',false);
 --Insertion Vendeur
 INSERT INTO sae3_skadjam._vendeur (id_compte,raison_sociale,siren,description_vendeur,iban,denomination) 
