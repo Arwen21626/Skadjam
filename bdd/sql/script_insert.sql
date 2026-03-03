5fd52b8afa9ef04230f1f73f7b3540426a7ce5a2
Julien <julien.lefrancois20@gmail.com>
2026-03-03 17:45:07 +0100

    ;

--- bdd/sql/script_insert.sql
+++ bdd/sql/script_insert.sql
@@ -163,7 +163,7 @@ VALUES
   ('rue de Charpentier',NULL,4,NULL,NULL,NULL,98306,'Pons-la-Forêt'),
   ('boulevard de Hamon',NULL,49,NULL,NULL,NULL,40361,'Guillotdan'),
   ('chemin Sylvie Meunier',NULL,5,NULL,NULL,NULL,32336,'Daniel'),
-  ('rue Louise Labbé',NULL,536,NULL,NULL,NULL,76820,'Sainte Alexnec');
+  ('rue Louise Labbé',NULL,536,NULL,NULL,NULL,76820,'Sainte Alexnec'),
   ('rue Anonymisé',NULL,1,NULL,NULL,NULL,00001,'Anonyme');
 
 --Insertion Habite
