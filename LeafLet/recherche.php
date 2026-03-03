9c777d9b06cf7ce2256905596e7488837f0efe27
Julien <julien.lefrancois20@gmail.com>
2026-03-03 15:53:04 +0100

    nom pour cocher

--- LeafLet/recherche.php
+++ LeafLet/recherche.php
@@ -113,8 +113,8 @@
                             $idVendeur = $v['id_compte'];
                             ?>
                             <div>
-                                <input class="vendeur h-5 w-5" type="checkbox" name="<?php echo $raisonSociale; ?>" id="<?php echo $idVendeur; ?>" value="<?php echo $idVendeur; ?>" class="triFiltre h-5 w-5">
-                                <label for="<?php echo $raisonSociale; ?>" class="labelDetails"><?php echo htmlspecialchars($raisonSociale, ENT_QUOTES, 'UTF-8'); ?></label>
+                                <input class="vendeur h-5 w-5" type="checkbox" name="<?php echo $idVendeur; ?>" id="<?php echo $idVendeur; ?>" value="<?php echo $idVendeur; ?>" class="triFiltre h-5 w-5">
+                                <label for="<?php echo $idVendeur; ?>" class="labelDetails"><?php echo htmlspecialchars($raisonSociale, ENT_QUOTES, 'UTF-8'); ?></label>
                             </div>
                         <?php } ?>
                     </details>
