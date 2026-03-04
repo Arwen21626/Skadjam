2f86b4140825879680a9ba022117c8a569b487e2
Anarkyia <s.salvadorhoarau@gmail.com>
2026-03-04 14:13:04 +0100

    ajout astérisque champ obligatoire

--- html/html/bo/creation_produit.php
+++ html/html/bo/creation_produit.php
@@ -378,7 +378,7 @@ if (isset($_POST['categorie']) && isset($_POST['nom']) && isset($_POST['prix']) 
                     </div>
                     <!-- Quantité par unité -->
                     <div class="flex flex-col">
-                        <label for="qteUnite">Quantité par unité :</label>
+                        <label for="qteUnite">Quantité par unité* :</label>
                         <input placeholder="200" class="border-4 border-beige rounded-2xl w-40 m-2 placeholder-gray-500" type="number" name="qteUnite" id="qteUnite" min="0" max="999999999"required>
                     </div>
                 </div>
