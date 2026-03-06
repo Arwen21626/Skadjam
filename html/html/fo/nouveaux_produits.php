b323d1da20e02f34e7c1b646238a669ccb88317c
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2026-03-06 16:48:18 +0100

    Nouveaux produits link à index

--- html/html/fo/nouveaux_produits.php
+++ html/html/fo/nouveaux_produits.php
@@ -37,7 +37,8 @@
 
     <main class="min-h-[600px] p-8">
         <!--Début du catalogue-->
-        <h2>Nos nouveaux produits</h2>
+        <h2 class="m-0">Nos nouveaux produits</h2>
+        <h3 class="text-center m-0 relative bottom-5">Produits ajoutées il y a moins de 30 jours</h3>
 
         <?php
             //initialisation du numéro de page
