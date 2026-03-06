916f58ed89989ab3ca793acb955c49d5abe4f31b
Diwezha <diwezha.ruveron@gmail.com>
2026-03-06 14:30:59 +0100

    fin corrections

--- html/html/bo/stock.php
+++ html/html/bo/stock.php
@@ -131,7 +131,7 @@
             console.log(lignesTab[i].children[4].children[0].textContent);
             
             // ajout d'une bordure qui indique que le stock est inférieur au seuil
-            if (lignesTab[i].children[4].children[0].textContent >= seuil){
+            if (lignesTab[i].children[4].children[0].textContent <= seuil){
                 
                 lignesTab[i].children[lignesTab[i].childElementCount-2].style.borderRightWidth = "0.5em";
 
