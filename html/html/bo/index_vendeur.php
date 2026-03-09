cede213f5aab9d70265108b11eec0d4d62f4484c
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2026-03-09 11:22:08 +0100

    changement alt boutons index

--- html/html/bo/index_vendeur.php
+++ html/html/bo/index_vendeur.php
@@ -49,16 +49,16 @@
     <main class="p-8">
         <div class="grid grid-cols-2 gap-4 justify-items-center">
             <a href="../bo/promotion_vendeur.php" title="lien vers page promotion" alt="promotion">
-                <img src="../../images/images_accueil/promotion.webp" title="lien vers page promotion" alt="promotion" class="w-150 h-auto justify-self-end">
+                <img src="../../images/images_accueil/promotion.webp" title="lien vers page promotion" alt="Vos produits en promotion" class="w-150 h-auto justify-self-end">
             </a>
             <a href="./details_produit.php?idProduit=<?php echo $dernierAjout['id_produit']; ?>" title="lien vers page derniers ajouts" alt="derniers ajouts">
-                <img src="../../images/images_accueil/derniers_ajouts.webp" title="lien vers le dernier produit ajoutés" alt="derniers ajouts" class="w-150 h-auto justify-self-start">
+                <img src="../../images/images_accueil/derniers_ajouts.webp" title="lien vers le dernier produit ajoutés" alt="Page du dernier produit que vous avez ajouté" class="w-150 h-auto justify-self-start">
             </a>           
             <a href="../bo/stock.php" title="lien vers page stock" alt="stock">
-                <img src="../../images/images_accueil/stock.webp" title="lien vers page stock" alt="stock" class="w-150 h-auto justify-self-end">
+                <img src="../../images/images_accueil/stock.webp" title="lien vers page stock" alt="Les Stocks de vos produits" class="w-150 h-auto justify-self-end">
             </a>
             <a href="../bo/liste_commandes.php" title="lien vers page commandes" alt="commandes">
-                <img src="../../images/images_accueil/commandes.webp" title="lien vers page commandes" alt="commandes" class="w-150 h-auto justify-self-start">
+                <img src="../../images/images_accueil/commandes.webp" title="lien vers page commandes" alt="Vos produits commandés par des clients" class="w-150 h-auto justify-self-start">
             </a>        
         </div>
 
