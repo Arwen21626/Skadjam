b999cc185a211682cd887b3e6dba298c055a2172
Anarkyia <s.salvadorhoarau@gmail.com>
2026-03-09 11:56:59 +0100

    fil d'ariane suite

--- html/html/fo/recapitulatif_commande.php
+++ html/html/fo/recapitulatif_commande.php
@@ -2,6 +2,7 @@
 session_start();
 require_once __DIR__ . "/../../php/verif_role_fo.php";
 require_once __DIR__ . "/../../php/fonctions.php";
+require_once __DIR__ . "/../../css/fo/fil_d_ariane.css";
 require(__DIR__ . '/../../01_premiere_connexion.php');
 
 // === Vérification utilisateur et panier ===
@@ -107,6 +108,7 @@ if (isset($_POST['valider'])) {
     <?php include __DIR__ . "/../../php/structure/navbar_front.php"; ?>
 
     <main class="min-h-[600px]">
+        <style></style>
         <ul class="etapes-processus-achat">
             <li class="actuel" etape="1">Récapitulatif commande</span></li>
             <li etape="2">Adresse de livraison</li>
