1c74a426317180c45415594e2e0928a74816a140
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-03-10 15:33:50 +0100

    Ajout dans la bdd des FA

---
+++ html/js/ajoutFAPanier.js
@@ -0,0 +1,5 @@
+// Pour le nom du fichier c'est pas ce que vous croyez
+let btnFA = document.getElementById("btnFA")
+let btnPanier = document.getElementById("btnPanier")
+
+btnFA.addEventListener
---
+++ html/php/traitementFAPanier.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+include __DIR__ . '/../01_premiere_connexion.php';
+// Déclaration des variables
+$idProd = $_GET['idProduit'];
+$ajout = $_GET['ajout'];
+$tabFABDD = [];
+$idCompte = -1;
+
+
+
+if ($ajout == "FA") {
+    if ($_SESSION['role'] == "visiteur") {
+        // Mis en clé pour éviter les doublons et retrouver plus vite
+        $_SESSION['futurAchat'][$idProd] = $idProd;
+    }
+    else{
+        $idCompte = $_SESSION['idCompte'];
+        // Récupération de la liste des futurs achats (FA) du client
+        foreach($dbh->query("SELECT id_produit FROM sae3_skadjam._futur_achat WHERE id_client = $idCompte", PDO::FETCH_ASSOC) as $row){
+            $tabFABDD[] = $row;
+        }
+        // Si le produit n'est pas déjà présent on l'ajoute
+        if (!in_array($idProd, $tabFABDD)) {
+            $insertFA = $dbh->prepare("INSERT INTO sae3_skadjam._futur_achat(id_produit, id_client) VALUES (?,?)");
+            $insertFA->execute([$idProd, $idCompte]);
+            echo "c'est dans la bdd";
+        }
+        // Sinon on le retire
+        // A faire plus tard
+    }
+}
+elseif ($ajout == "Panier") {
+    echo "Alède";
+}
+?>
+
+
