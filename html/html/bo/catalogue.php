8e34f85f872f56425c7444d0afdde4beb8ccd980
Armelker197 <armel.kermanach@etudiant.univ-rennes.fr>
2026-03-12 09:12:06 +0100

    catalogue maquette

---
+++ html/html/bo/catalogue.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php require_once __DIR__ . "/../../php/structure/head_back.php" ?>
+<head>
+    <title>Créer un catalogue</title>
+</head>
+<body>
+    <?php 
+        require_once __DIR__ . "/../../php/structure/header_back.php";
+        require_once __DIR__ . "/../../php/structure/navbar_back.php";
+    ?>
+    <main>
+        
+    </main>
+    <?php
+        require_once __DIR__ . "/../../php/structure/footer_back.php";
+    ?>
+</body>
+</html>
