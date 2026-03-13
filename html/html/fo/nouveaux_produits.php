58f411742bc9c67ebb8126859133cbf4089e36d2
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-03-13 11:56:28 +0100

    réparation icones panier et fa sur nouveaux produit

--- html/html/fo/nouveaux_produits.php
+++ html/html/fo/nouveaux_produits.php
@@ -20,8 +20,9 @@ if ($_SESSION['role'] == "visiteur") {
     $tabFA = $_SESSION['futurAchat'];
 }
 
-// Récupération des futurs achats du client
+// Récupération des futurs achats et du panier du client
 include __DIR__. '/../../php/requetesBDD/recup_FA.php';
+include __DIR__. '/../../php/requetesBDD/recup_panier.php';
 ?>
 
 <!DOCTYPE html>
@@ -117,9 +118,9 @@ include __DIR__. '/../../php/requetesBDD/recup_FA.php';
                                     <?php 
                                     $bg = "bg-[url(/images/logo/bootstrap_icon/bookmark-fa-plus.svg)]";
                                     
-                                    $trouve = array_search($idProduit, $tabFA);
+                                    $trouveFA = array_search($idProduit, $tabFA);
                                     
-                                    if ($trouve != null) {
+                                    if ($trouveFA != null) {
                                         $bg = "bg-[url(/images/logo/bootstrap_icon/bookmark-fa-plus-fill.svg)]";
                                     }
                                     echo $bg;
@@ -127,7 +128,25 @@ include __DIR__. '/../../php/requetesBDD/recup_FA.php';
                                     ">
                                     </button>
                                 </a>
-                                <a id="btnPanier" href=""><button class="cursor-pointer size-10 bg-no-repeat bg-size-[auto_40px] bg-[url(/images/logo/bootstrap_icon/cart-vert-fonce.svg)] hover:bg-[url(/images/logo/bootstrap_icon/cart-fill-vert-fonce.svg)]"></button></a>
+                                <!-- Produit dans le panier ? -->
+                                <a id="btnPanier" href="/php/traitementFAPanier.php?idProduit=<?php echo $idProduit;?>&ajout=panier&vientDe=nP">
+                                    <button class="cursor-pointer size-10 bg-no-repeat bg-size-[auto_40px]
+                                        <?php 
+                                        $bg = "bg-[url(/images/logo/bootstrap_icon/cart-vert-fonce.svg)]";
+                                        $trouveP = false;
+
+                                        if (isset($_SESSION['panier']['contient'][$idProduit])){
+                                            $trouveP = true;
+                                        }
+
+                                        if ($trouveP != false) {
+                                            $bg = "bg-[url(/images/logo/bootstrap_icon/cart-fill-vert-fonce.svg)]";
+                                        }
+                                        echo $bg;
+                                        ?>
+                                    ">
+                                    </button>
+                                </a>
                             </div>  
                             <!--affichage de la promotion-->
                                 <?php if($estPromu){ 
