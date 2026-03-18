8ae64231ae97b6c6f232b03884c330dcefaf775a
Arwen21626 <jobit-b.arwen21@bbox.fr>
2026-03-18 11:55:52 +0100

    Changer le style des inputs des popups

--- html/html/fo/futurs_achats.php
+++ html/html/fo/futurs_achats.php
@@ -250,7 +250,7 @@ $lignes = array_slice($tabProd, $pageNumber*PAGE_SIZE-PAGE_SIZE, PAGE_SIZE);
             <form action="/php/traitementFAPanier.php" method="get" id="formNbAddPanier" class="flex flex-col items-center w-full h-full space-y-4">
                 <label id="validAjout" class="hidden" for="nbAddPanier">Combien voulez-vous en ajouter au panier ?</label>
                 <p id="valideRetrait" class="hidden">Etes-vous sur de vouloir retirer ce produit de votre panier ?</p>
-                <input placeholder="5" class="hidden pl-3 border-4 border-beige rounded-2xl w-20 m-2 placeholder-gray-500" type="number" name="nbAddPanier" id="nbAddPanier" min="1">
+                <input placeholder="5" class="hidden pl-3 border-4 border-vertClair rounded-2xl w-20 m-2 placeholder-gray-500" type="number" name="nbAddPanier" id="nbAddPanier" min="1">
                 <input type="hidden" name="ajout" value="panier">
                 <input type="hidden" name="vientDe" value="fa">
                 <div class="flex flex-row justify-around w-full">
